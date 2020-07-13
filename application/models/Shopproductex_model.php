2d9722341090c108709afed8ea1cdb870cdadb7f
aktwork <larastar721@gmail.com>
2020-07-13 14:01:59 +0200

    solved group_concat min length 1024

--- application/models/Shopproductex_model.php
+++ application/models/Shopproductex_model.php
@@ -69,97 +69,6 @@
             return true;
         }
 
-        // public function getUserProductsDetailsPublic(int $spotId, int $userId): ?array
-        // {
-        //     $time = time();
-        //     $date = date('Y-m-d H:i:s', $time);
-        //     $day = date('D', time());
-        //     $hours = date('H:i:s', $time);
-
-        //     $query =
-        //         "SELECT
-        //             `tbl_shop_products_extended`.`id` as `productExtendedId`,
-        //             `tbl_shop_products_extended`.`name`,
-        //             `tbl_shop_products_extended`.`shortDescription`,
-        //             `tbl_shop_products_extended`.`longDescription`,
-        //             FORMAT(tbl_shop_products_extended.price, 2) AS price,
-        //             `tbl_shop_products_extended`.`image`,
-        //             `tbl_shop_products`.`id` AS `productId`,
-        //             `tbl_shop_products`.`stock`,
-        //             `tbl_shop_products`.`recommendedQuantity`,
-        //             `tbl_shop_products`.`active` AS `productActive`,
-        //             `tbl_shop_products`.`showImage`,
-        //             `tbl_shop_products`.`dateTimeFrom`,
-        //             `tbl_shop_products`.`dateTimeTo`,
-        //             `tbl_shop_categories`.`category`,
-        //             `tbl_shop_categories`.`id` AS `categoryId`,
-        //             `tbl_shop_categories`.`active` AS `categoryActive`,
-        //             `tbl_shop_products_types`.`type` AS `productType`
-        //         FROM
-        //             `tbl_shop_products_extended`
-        //             INNER JOIN `tbl_shop_products` ON `tbl_shop_products_extended`.`productId` = `tbl_shop_products`.`id`
-        //             INNER JOIN `tbl_shop_categories` ON `tbl_shop_products`.`categoryId` = `tbl_shop_categories`.`id`
-        //             INNER JOIN
-        //                 (
-        //                     SELECT
-        //                         tbl_shop_product_times.productId
-        //                     FROM
-        //                         tbl_shop_product_times
-        //                     WHERE
-        //                         tbl_shop_product_times.day = '{$day}'
-        //                         AND
-        //                         tbl_shop_product_times.timeFrom <= '{$hours}'
-        //                         AND
-        //                         tbl_shop_product_times.timeTo > '{$hours}'
-        //                 ) productTimes ON productTimes.productId = tbl_shop_products.id
-        //         WHERE
-        //             `tbl_shop_categories`.`active` = '1'
-        //             AND `tbl_shop_categories`.`userId` = '{$userId}'
-        //             AND `tbl_shop_products`.`active` = '1'
-
-
-        //             AND
-        //             (
-        //                 (`tbl_shop_products`.`dateTimeFrom` <= '{$date}' AND `tbl_shop_products`.`dateTimeTo` > '{$date}' )
-        //                 OR
-        //                 (`tbl_shop_products`.`dateTimeFrom` IS NULL AND `tbl_shop_products`.`dateTimeTO` IS NULL)
-        //             )
-
-        //         ORDER BY `tbl_shop_products_extended`.`id` DESC";
-        //         echo $query; die();
-        //         $result = $this->db->query($query);
-        //         $result = $result->result_array();
-        //         return $result ? $result : null;
-        // }
-
-        // public function getUserLastProductsDetailsPublic(int $spotId, int $userId, string $sortBy = 'name'): ?array
-        // {
-        //     $products = $this->getUserProductsDetailsPublic($spotId, $userId);
-
-        //     var_dump($products); die();
-        //     if (is_null($products)) return null;
-
-        //     $this->load->model('shopprinters_model');
-        //     $this->load->helper('utility_helper');
-
-        //     $productIds = [];
-        //     $return = [];
-        //     foreach ($products as $prodcut) {
-        //         if (!in_array($prodcut['productId'], $productIds)) {
-        //             // $prodcut['printers'] = $this->shopprinters_model->fetchtProductPrinters(intval($prodcut['productId']));
-        //             // if ($prodcut['printers']) {
-        //             //     $prodcut['printers'] = Utility_helper::resetArrayByKeyMultiple($prodcut['printers'], 'printerId');
-        //             // }
-        //             array_push($return, $prodcut);
-        //             array_push($productIds, $prodcut['productId']);
-        //         }
-        //     }
-        //     $return = Utility_helper::resetArrayByKeyMultiple($return, $sortBy);
-        //     ksort($return);
-        //     return $return;
-        // }
-
-
         public function getUserProducts(int $userId): ?array
         {
             $this->load->config('custom');
@@ -262,8 +171,8 @@
                                     \'|\', tbl_shop_products_types.isMain,
                                     \'|\',' . $this->table. '.updateCycle,
                                     \'|\',' . $this->table. '.showInPublic
-                                    
-                                    SEPARATOR "'. $this->config->item('contactGroupSeparator') . '"
+                                    ORDER BY ' . $this->table. '.id DESC
+                                    SEPARATOR "'. $this->config->item('contactGroupSeparator') . '"                                    
                                 ) AS productDetails
                             FROM
                                 ' . $this->table . '
@@ -271,8 +180,9 @@
                                 tbl_shop_products_types ON ' . $this->table . '.productTypeId = tbl_shop_products_types.id
 
                             GROUP BY ' . $this->table. '.productId
+                            ORDER BY ' . $this->table. '.productTypeId DESC
                             
-                        ) tblShopProductDetails',                    
+                        ) tblShopProductDetails',
                         'tblShopProductDetails.productTypeId = tbl_shop_products_types.id AND tblShopProductDetails.name = ' . $this->table . '.name',
                         'INNER'
                     ]
@@ -293,6 +203,7 @@
                 $products[$key]['productTimes'] = $this->prepareProductTimes($product['productTimes'], $this->config->item('contactGroupSeparator'));
                 $products[$key]['productDetails'] = $this->prepareProductDetails($product['productDetails'], $this->config->item('contactGroupSeparator'));
             }
+
             $products = Utility_helper::resetArrayByKeyMultiple($products, $resetBy);
             return $products;
         }
@@ -335,36 +246,26 @@
                 return explode('|', $data);
             }, $productDetails);
 
-            $cycle = 0;
             $return = [];
-            $productExtendedIds = []; //TIQS TO DO CHECK WHY WE GET DOUBLE DATA
 
             foreach($productDetails as $index => $details) {
+                if ($index > 0 && $productDetails[$index][8] !== $productDetails[$index - 1][8]) break;
 
-                $detailsCycle = intval($details[8]);
-                if ($cycle < $detailsCycle) {
-                    $cycle = $detailsCycle;
-                }
-
-                if (!in_array($details[0], $productExtendedIds)) {
-                    $collect = [
-                        'productExtendedId'     => $details[0],
-                        'name'                  => $details[1],
-                        'shortDescription'      => $details[2],
-                        'price'                 => $details[3],
-                        'vatpercentage'         => $details[4],
-                        'productTypeId'         => $details[5],
-                        'productType'           => $details[6],
-                        'productTypeIsMain'     => $details[7],
-                        'productUpdateCycle'    => $details[8],
-                        'showInPublic'          => $details[9]
-                    ];
-                    array_push($return, $collect);
-                    array_push($productExtendedIds, $details[0]);
-                }
+                $collect = [
+                    'productExtendedId'     => $details[0],
+                    'name'                  => $details[1],
+                    'shortDescription'      => $details[2],
+                    'price'                 => $details[3],
+                    'vatpercentage'         => $details[4],
+                    'productTypeId'         => $details[5],
+                    'productType'           => $details[6],
+                    'productTypeIsMain'     => $details[7],
+                    'productUpdateCycle'    => $details[8],
+                    'showInPublic'          => $details[9]
+                ];
+                array_push($return, $collect);
             }
 
-            $return = Utility_helper::resetArrayByKeyMultiple($return, 'productUpdateCycle')[$cycle];
             return $return;
         }
     }
