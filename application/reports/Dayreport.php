ca7306c816bd08e2890922f81bb47b2bc3c4c058
aktwork <larastar721@gmail.com>
2020-08-18 03:17:15 +0200

    Working on dashboard changed the makeorder select type

--- application/reports/Dayreport.php
+++ application/reports/Dayreport.php
@@ -57,6 +57,71 @@
                 ])
                 ->pipe($this->dataStore('dayreport_report'))
                 ;
+
+            ///
+
+			$this
+				->src('alfred')
+				->query('
+						SELECT
+							SUM( tbl_shop_orders.serviceFee ) as servicefee,
+							DATE( tbl_shop_orders.created ) as DateCreated,
+							SUM( tbl_shop_orders.amount ) as amount,
+							SUM( tbl_shop_orders.amount + tbl_shop_orders.serviceFee ) AS total,
+							tbl_shop_spots.spotName,
+							tbl_user.username,
+							tbl_user.first_name,
+							tbl_user.id 
+						FROM
+							tbl_shop_orders
+							INNER JOIN tbl_shop_spots ON tbl_shop_orders.spotId = tbl_shop_spots.id
+							INNER JOIN tbl_shop_printers ON tbl_shop_spots.printerId = tbl_shop_printers.id
+							INNER JOIN tbl_user ON tbl_shop_printers.userId = tbl_user.id 
+						WHERE
+							tbl_user.id = :vendorId
+							AND tbl_shop_orders.paid = \'1\' 
+							AND DATE(tbl_shop_orders.created) = CURRENT_DATE() 
+						GROUP BY
+							DATE( tbl_shop_orders.created ) 
+
+                ')
+				->params([
+					":vendorId" => $this->params["vendorId"]
+				])
+				->pipe($this->dataStore('dayreport_day'))
+			;
+
+			$this
+				->src('alfred')
+				->query('
+						SELECT
+							SUM( tbl_shop_orders.serviceFee ) as servicefee,
+							DATE( tbl_shop_orders.created ) as DateCreated,
+							SUM( tbl_shop_orders.amount ) as amount,
+							SUM( tbl_shop_orders.amount + tbl_shop_orders.serviceFee ) AS total,
+							tbl_shop_spots.spotName,
+							tbl_user.username,
+							tbl_user.first_name,
+							tbl_user.id 
+						FROM
+							tbl_shop_orders
+							INNER JOIN tbl_shop_spots ON tbl_shop_orders.spotId = tbl_shop_spots.id
+							INNER JOIN tbl_shop_printers ON tbl_shop_spots.printerId = tbl_shop_printers.id
+							INNER JOIN tbl_user ON tbl_shop_printers.userId = tbl_user.id 
+						WHERE
+							tbl_user.id = :vendorId
+							AND tbl_shop_orders.paid = \'1\' 
+							AND WEEK(tbl_shop_orders.created,1) = WEEK(CURRENT_DATE(),1) 
+						GROUP BY
+							DATE( tbl_shop_orders.created ) 
+
+                ')
+				->params([
+					":vendorId" => $this->params["vendorId"]
+				])
+				->pipe($this->dataStore('dayreport_week'))
+			;
+
         }
     }
 
--- application/reports/Dayreport.view.php
+++ application/reports/Dayreport.view.php
@@ -4,102 +4,220 @@
 	use \koolreport\amazing\Theme;
 ?>
 
-<div class="main-wrapper-nh">
+<div class="main-wrapper">
 	<div class="col-half background-apricot height-100" style="text-align:left; font-size: smaller">
-<div class="text-center" style="margin-bottom: -30px; margin-top: -50px">
-	</div>
-		<div class="report-content" >
-		<?php
-		\koolreport\amazing\DualChartCard::create(array(
-			"title"=>"TOTAL INCOME",
+		<div class="align-top width-650">
+			<div class="report-content" >
+				<?php
+				\koolreport\amazing\DualChartCard::create(array(
+					"title"=>"DAY TOTAL",
 
-			"value"=>$this->dataStore("dayreport_report")->sum("total"),
-			"prefix" => "€ ",
-			"cssClass"=>array(
-				"icon"=>"fa fa-euro"
-			),
-			"cssStyle"=> [
-				"card"=>"background-color:Blue",
-				"title"=>"font-weight:bold",
-				"value"=>"font-style:italic",
-				"icon"=>"font-size:24px;color:white"
-			],
-			"chart"=>array(
-				"type"=>"area",
-				"dataSource"=>$this->dataStore("dayreport_report")->sortKeysDesc("DateCreated"),
-				"columns"=>array(
-					"DateCreated",
-					"amount"=>array(
-						"type"=>"number",
-						"decimals"=>2,              // Number of decimals to show
-						"decimalPoint"=>".",        // Decimal point character
-						"thounsandSeparator"=>",",  // Thousand separator
-						"prefix"=>"€"
-					)
-				)
-			),
-			    "secondChart"=>array(
-					"dataSource"=>$this->dataStore("dayreport_report")->sortKeysDesc("DateCreated"),
-				"columns"=>array(
-					"DateCreated",
-					"amount"=>array(
-						"type"=>"number",
-						"decimals"=>2,              // Number of decimals to show
-						"decimalPoint"=>".",        // Decimal point character
-						"thounsandSeparator"=>",",  // Thousand separator
-						"prefix"=>"€"
-					)
-				)
-			),
+					"value"=>$this->dataStore("dayreport_day")->sum("total"),
+					"format"=>array(
+						"value"=>array(
+							"type"=>"number",
+							"decimals"=>2,              // Number of decimals to show
+							"decimalPoint"=>",",        // Decimal point character
+							"thounsandSeparator"=>".",  // Thousand separator
+							"prefix"=>"€ ",
+						)
+					),
+					"cssClass"=>array(
+						"icon"=>"fa fa-euro"
+					),
+					"cssStyle"=> [
+						"card"=>"background-color:#138575",
+						"title"=>"font-weight:bold",
+						"value"=>"font-style:italic",
+						"icon"=>"font-size:24px;color:white"
+					],
+					"chart"=>array(
+						"type"=>"area",
+						"dataSource"=>$this->dataStore("dayreport_report")->sortKeysDesc("DateCreated"),
+						"columns"=>array(
+							"DateCreated",
+							"amount"=>array(
+								"label"=>"Amount",
+								"type"=>"number",
+								"decimals"=>2,              // Number of decimals to show
+								"decimalPoint"=>".",        // Decimal point character
+								"thounsandSeparator"=>",",  // Thousand separator
+								"prefix"=>"€"
+							)
+						)
+					),
+						"secondChart"=>array(
+							"dataSource"=>$this->dataStore("dayreport_report")->sortKeysDesc("DateCreated"),
+						"columns"=>array(
+							"DateCreated",
+							"amount"=>array(
+								"type"=>"number",
+								"decimals"=>2,              // Number of decimals to show
+								"decimalPoint"=>".",        // Decimal point character
+								"thounsandSeparator"=>",",  // Thousand separator
+								"prefix"=>"€"
+							)
+						)
+					),
 
-		));
-		?>
+				));
+				?>
+			</div>
+			<div class="report-content" style="margin-bottom: -30px; margin-top: 30px">
+				<?php
+				DataTables::create(array(
+					"responsive"=>true,
+					"dataSource"=>$this->dataStore("dayreport_day")->sortKeysDesc("DateCreated"),
+
+					"themeBase"=>"bs4", // Optional option to work with Bootsrap 4
+					"cssClass"=>array(
+						"table"=>"table table-striped table-bordered"
+					),
+					'width' => '100%',
+					"columns"=>array(
+						"DateCreated"=>array(
+							"label"=> "Date-day",
+							"type"=>"date"
+						),
+						"total"=>array(
+							"label"=> "Total",
+							"type"=>"number",
+							"decimals"=>2,
+							"prefix"=>"€ "
+						),
+
+	//					"amount"=>array(
+	//					   "type"=>"number",
+	//						 "decimals"=>2
+	//					 ),
+	//					"servicefee"=>array(
+	//					   "type"=>"number",
+	//						 "decimals"=>2
+	//					 )
+
+					 ),
+
+					"options"=>array(
+						"order"=>array(
+							array(0,"desc"), //Sort by first column desc
+							array(1,"asc") //Sort by second column asc
+						),
+						"searching"=>true,
+						"colReorder"=>true,
+					),
+
+				));
+				?>
+			</div>
 		</div>
-		<div class="report-content" style="margin-bottom: -30px; margin-top: -50px">
-			<?php
-			DataTables::create(array(
-				"responsive"=>true,
-				"dataSource"=>$this->dataStore("dayreport_report")->sortKeysDesc("DateCreated"),
+	</div>
+	<div class="col-half background-apricot height-100" style="text-align:left; font-size: smaller">
+		<div class="align-top width-650" >
+			<div class="report-content" >
+				<?php
+				\koolreport\amazing\DualChartCard::create(array(
+					"title"=>"THIS WEEK TOTAL",
 
-				"themeBase"=>"bs4", // Optional option to work with Bootsrap 4
-				"cssClass"=>array(
-					"table"=>"table table-striped table-bordered"
-				),
-				'width' => '100%',
-				"columns"=>array(
-					"DateCreated"=>array(
-						"label"=> "Date-day",
-						"type"=>"date"
+					"value"=>$this->dataStore("dayreport_week")->sum("total"),
+					"format"=>array(
+						"value"=>array(
+							"type"=>"number",
+							"decimals"=>2,              // Number of decimals to show
+							"decimalPoint"=>",",        // Decimal point character
+							"thounsandSeparator"=>".",  // Thousand separator
+							"prefix"=>"€ ",
+						)
 					),
-					"total"=>array(
-						"label"=> "Total",
-						"type"=>"number",
-						"decimals"=>2,
-						"prefix"=>"€ "
+				    "cssClass"=>array(
+						"card"=>"my-own-card-class",
+						"tittle"=>"font-bold",
+						"value"=>"big-font",
+						"icon"=>"fa fa-dollar"
+					),
+					"cssStyle"=> [
+						"card"=>"background-color:#04a7b8",
+						"title"=>"font-weight:bold",
+						"value"=>"font-style:italic",
+						"icon"=>"font-size:24px;color:white"
+					],
+
+					"chart"=>array(
+					"height"=>"300px",
+						"dataSource"=>$this->dataStore("dayreport_week")->sortKeysDesc("DateCreated"),
+						"columns"=>array(
+							"DateCreated",
+							"amount"=>array(
+								"type"=>"number",
+								"decimals"=>2,              // Number of decimals to show
+								"decimalPoint"=>".",        // Decimal point character
+								"thounsandSeparator"=>",",  // Thousand separator
+								"prefix"=>"€",
+							)
+						)
+					),
+					"secondChart"=>array(
+						"dataSource"=>$this->dataStore("dayreport_week")->sortKeysDesc("DateCreated"),
+						"columns"=>array(
+							"DateCreated",
+							"amount"=>array(
+								"type"=>"number",
+								"decimals"=>2,              // Number of decimals to show
+								"decimalPoint"=>".",        // Decimal point character
+								"thounsandSeparator"=>",",  // Thousand separator
+								"prefix"=>"€"
+							)
+						)
 					),
 
-//					"amount"=>array(
-//					   "type"=>"number",
-//						 "decimals"=>2
-//					 ),
-//					"servicefee"=>array(
-//					   "type"=>"number",
-//						 "decimals"=>2
-//					 )
+				));
+				?>
+			</div>
+			<div class="report-content" style="margin-bottom: -30px; margin-top: 30px">
+				<?php
+				DataTables::create(array(
+					"responsive"=>true,
+					"dataSource"=>$this->dataStore("dayreport_week")->sortKeysDesc("DateCreated"),
 
-   				 ),
-
-				"options"=>array(
-					"order"=>array(
-						array(0,"desc"), //Sort by first column desc
-						array(1,"asc") //Sort by second column asc
+					"themeBase"=>"bs4", // Optional option to work with Bootsrap 4
+					"cssClass"=>array(
+						"table"=>"table table-striped table-bordered"
 					),
-					"searching"=>true,
-					"colReorder"=>true,
-				),
+					'width' => '100%',
+					"columns"=>array(
+						"DateCreated"=>array(
+							"label"=> "Date-day",
+							"type"=>"date"
+						),
+						"total"=>array(
+							"label"=> "Total",
+							"type"=>"number",
+							"decimals"=>2,
+							"prefix"=>"€ "
+						),
 
-			));
-			?>
+						//					"amount"=>array(
+						//					   "type"=>"number",
+						//						 "decimals"=>2
+						//					 ),
+						//					"servicefee"=>array(
+						//					   "type"=>"number",
+						//						 "decimals"=>2
+						//					 )
+
+					),
+
+					"options"=>array(
+						"order"=>array(
+							array(0,"desc"), //Sort by first column desc
+							array(1,"asc") //Sort by second column asc
+						),
+						"searching"=>true,
+						"colReorder"=>true,
+					),
+
+				));
+				?>
+			</div>
 		</div>
 	</div>
 </div>
