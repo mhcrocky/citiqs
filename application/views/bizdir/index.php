2ddcdca4be2ab21bd4d6894cdca6a57448bfe3f0
aktwork <larastar721@gmail.com>
2020-10-21 14:24:16 +0200

    modified range slider

--- application/views/bizdir/index.php
+++ application/views/bizdir/index.php
@@ -1,39 +1,77 @@
+<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css">
 <style>
-
-input[type='range'] {
- -webkit-appearance: none;
- border-radius: 5px;
- box-shadow: inset 0 0 5px #333;
- background-color: #999;
- height: 10px;
- vertical-align: middle;
+.ui-slider, .ui-slider .slider-range-inverse, .ui-slider .ui-slider-range {
+  height: 14px;
+  border-radius: 10px;
+  border-width: 0;
 }
-input[type='range']::-moz-range-track {
-  -moz-appearance: none;
+
+
+#slider-container {
+  width: 100%;
+  height: 80px;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  position: relative;
+  top: 50%;
+  margin: 0 auto;
+  text-align: center;
+  background: #FFF;
   border-radius: 5px;
-  box-shadow: inset 0 0 5px #333;
-  background-color: #999;
-  height: 10px;
-}
-input[type='range']::-webkit-slider-thumb {
-  -webkit-appearance: none !important;
-  border-radius: 20px;
-  background-color: #FFF;
-  box-shadow:inset 0 0 10px rgba(000,000,000,0.5);
-  border: 1px solid #999;
-  height: 20px;
-  width: 20px;
-}
-input[type='range']::-moz-range-thumb {
-  -moz-appearance: none;
-  border-radius: 20px;
-  background-color: #FFF;
-  box-shadow:inset 0 0 10px rgba(000,000,000,0.5);
-  border: 1px solid #999;
-  height: 20px;
-  width: 20px;
+  padding: 35px 40px 30px 40px;
+  box-shadow: 0 10px 30px rgba(0, 0, 0, 0.2);
+  margin-bottom:25px;
 }
 
+.ui-slider {
+  background-color: #1ABC9C;
+  background-image: -webkit-linear-gradient(left, #1ABC9C 0%, #F1C40F 50%, #E74C3C 100%);
+  background-image: linear-gradient(to right,#1ABC9C 0%, #F1C40F 50%, #E74C3C 100%);
+}
+.ui-slider * {
+  outline: none;
+}
+.ui-slider .slider-range-inverse {
+  background: #CCC;
+  position: absolute;
+  right: 0;
+}
+.ui-slider .ui-slider-range {
+  background: transparent;
+}
+.ui-slider .ui-slider-handle {
+  width: 28px;
+  height: 28px;
+  cursor: pointer;
+  box-shadow: 0 3px 8px rgba(0, 0, 0, 0.4);
+  background: #FFF;
+  top: -7px;
+  border-radius: 50%;
+  border-width: 0;
+}
+.ui-slider .ui-slider-handle:active {
+  box-shadow: 0 3px 20px rgba(0, 0, 0, 0.5);
+}
+.ui-slider .ui-slider-handle .dot {
+  width: 18px;
+  height: 18px;
+  border-radius: 50%;
+  position: absolute;
+  top: 5px;
+  left: 5px;
+  background: transparent;
+  overflow: hidden;
+}
+.ui-slider .ui-slider-handle .dot .handle-track {
+  display: block;
+  height: 18px;
+  background-color: #1ABC9C;
+  background-image: -webkit-linear-gradient(left, #1ABC9C 0%, #F1C40F 50%, #E74C3C 100%);
+  background-image: linear-gradient(to right,#1ABC9C 0%, #F1C40F 50%, #E74C3C 100%);
+  position: absolute;
+  padding-right: 18px;
+}
 </style>
 <main role="main" style="margin-bottom: 0px">
 	<section class="jumbotron" style="background-color:#F3D0B5">
@@ -74,9 +112,17 @@ input[type='range']::-moz-range-thumb {
 					</div>
 					
 			</div>
-			<input type="range" id="myRange" value="1" max="100">
+
+			<div id="slider-container">
+				<div id="js-slider" class="ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all"><div class="slider-range-inverse" style="width: 51%;"></div><div class="ui-slider-range ui-widget-header ui-corner-all ui-slider-range-min" style="width: 49%;"></div><span class="ui-slider-handle ui-state-default ui-corner-all" tabindex="0" style="left: 49%;"><span class="dot"><span class="handle-track" style="width: 1000px; left: -490px;"></span></span></span></span></div>
+				<span id="rangeValue">1 km</span>
+			</div>
+			<input type="hidden" id="myRange" value="1">
+
+
+			<!--<input type="range" id="myRange" value="1" max="100">
 					<div style="visibility:hidden" id="rangeValue">1</div>
-			<div class="row" id="placesContainer">
+			<div class="row" id="placesContainer">-->
 			
 				<?php foreach ($directories as $directory): ?>
 					<div
@@ -125,8 +171,58 @@ input[type='range']::-moz-range-thumb {
 		</div>
 	</div>
 </main>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
 <script>
 $('#myRange').mousemove(function(){
     $('#rangeValue').text($('#myRange').val());
 });
-</script>
+
+(function () {
+  // Helper function
+  var update_handle_track_pos;
+
+  update_handle_track_pos = function (slider, ui_handle_pos) {
+    var handle_track_xoffset, slider_range_inverse_width;
+    handle_track_xoffset = -(ui_handle_pos / 100 * slider.clientWidth);
+    $(slider).find(".handle-track").css("left", handle_track_xoffset);
+    slider_range_inverse_width = 100 - ui_handle_pos + "%";
+    return $(slider).find(".slider-range-inverse").css("width", slider_range_inverse_width);
+  };
+
+  // Init slider
+  $("#js-slider").slider({
+    range: "min",
+    max: 100,
+    value: 1,
+    create: function (event, ui) {
+      var slider;
+      slider = $(event.target);
+	 
+
+      // Append the slider handle with a center dot and it's own track
+      slider.find('.ui-slider-handle').append('<span class="dot"><span class="handle-track"></span></span>');
+
+      // Append the slider with an inverse range
+      slider.prepend('<div class="slider-range-inverse"></div>');
+
+      // Set initial dimensions
+      slider.find(".handle-track").css("width", event.target.clientWidth);
+
+      // Set initial position for tracks
+      return update_handle_track_pos(event.target, $(this).slider("value"));
+    },
+    slide: function (event, ui) {
+      // Update position of tracks
+	  $('#rangeValue').text(ui.value+' km');
+	  $('#myRange').val(ui.value);
+      return update_handle_track_pos(event.target, ui.value);
+    } });
+	
+
+
+
+}).call(this);
+
+
+    </script>
