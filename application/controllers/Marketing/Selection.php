83a9a0d1285e35f9f125bc9d38af7985acc51b54
aktwork <larastar721@gmail.com>
2020-10-23 13:03:07 +0200

    first commit

---
+++ application/controllers/Marketing/Selection.php
@@ -0,0 +1,59 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . '/libraries/BaseControllerWeb.php';
+
+class Selection extends BaseControllerWeb {
+
+    
+    public function __construct(){
+        parent::__construct();
+        if (empty($this->session->userdata('userId'))) {
+			redirect('login');
+		}
+    }
+    public function index() {
+        $this->global['pageTitle'] = 'TIQS BUYERS';
+        //var_dump($this->Selection_model->get_buyers(1162));
+        $this->loadViews("marketing/selection", $this->global, '', 'footerpublicbizdir', 'noheaderbizdir');
+    }
+
+    public function sendMessage() {
+        $buyerId = $this->input->post('buyerId');
+        $buyerMobile = $this->input->post('buyerMobile');
+        $buyerOneSignalId = $this->input->post('buyerOneSignalId');
+        $message = $this->input->post('message');
+        if($buyerOneSignalId == 'null'){
+            $this->load->library('Sms');
+            $sms = new Sms;
+            $sms->send($buyerMobile,$message);
+            echo 'sms success';
+        } else {
+            $this->load->library('Notification');
+            $notification = new Notification;
+            $notification->sendMessage($buyerOneSignalId,$message);
+            echo 'success';
+        }
+
+    }
+
+    public function allbuyers(){
+        $this->load->model('Selection_model');
+        $userId = $this->session->userdata('userId');
+        $buyers = $this->Selection_model->get_buyers($userId);
+        /*
+        return $this->output
+					->set_content_type('application/json')
+					->set_status_header(200)
+                    ->set_output(json_encode($buyers));
+        */
+        echo json_encode($buyers);
+    }
+
+    public function test() {
+        $this->global['pageTitle'] = 'TIQS BUYERS';
+        //var_dump($this->Selection_model->get_buyers(1162));
+        $this->loadViews("marketing/test", $this->global, '', 'footerpublicbizdir', 'noheaderbizdir');
+    }
+}
