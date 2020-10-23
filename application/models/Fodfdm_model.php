fb8f6205f05bb071e88dcd44fd299d0ff3e8e906
aktwork <larastar721@gmail.com>
2020-10-23 23:43:16 +0500

    rename fodfdm model

--- application/models/Fodfdm_model.php
+++ application/models/Fodfdm_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class FOD_FDM_Model extends CI_Model {
+class Fodfdm_model extends CI_Model {
 
 	function getlastRecieptCount($vendorId){
 		$this->db->where('vendorId',$vendorId);
