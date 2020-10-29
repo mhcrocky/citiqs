69a3b03fb7ad7bfe38efecaabc5b0f8065d5c50c
aktwork <larastar721@gmail.com>
2020-10-30 00:24:48 +0100

    added get_bizdir_by_location function

--- application/models/Bizdir_model.php
+++ application/models/Bizdir_model.php
@@ -16,6 +16,30 @@ class Bizdir_model extends CI_Model {
 		return $query->result_array();
 	}
 
+	public function get_bizdir_by_location($location)
+	{
+		if(strstr($location, ',')){
+			$locations = explode(',' , $location);
+			//var_dump($arr);
+			$address = $locations[0];
+			$city = $locations[1];
+			$this->db->like('address', $address);
+			$this->db->like('city', $city);
+		} else {
+			$this->db->or_like('address', $location);
+			$this->db->or_like('city', $location);
+			$this->db->or_like('zipcode', $location);
+			
+		}
+		//$this->db->or_like('address', $address);
+		$this->db->where('roleid', 2);
+		$this->db->where('IsDropOffPoint', 1);
+		$this->db->where('address !=', '');
+		$this->db->where('bizdir', 1);
+		$query = $this->db->get('tbl_user');
+		return $query->result_array();
+	}
+
 	public function set_bizdir()
 	{
 		$data = array(
