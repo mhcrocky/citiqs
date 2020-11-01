f32078c21169a1ae6a94770ebbd205545ee36c73
aktwork <larastar721@gmail.com>
2020-11-01 17:13:29 +0100

    added sendEmail and sendEmailTemplate functions

--- application/controllers/Marketing/Calculator.php
+++ application/controllers/Marketing/Calculator.php
@@ -26,7 +26,56 @@ class Calculator extends BaseControllerWeb {
 
         $this->load->model('Calculator_model');
         $this->Calculator_model->save($data);
+        $this->sendEmailTemplate($data);
         
+    }
+
+    public function sendEmailTemplate($data = array()){
+        $user_id = 1;
+        $temp_name = 'calculateemail';
+        $this->load->model('email_templates_model');
+        $emailTemplate = $this->email_templates_model->get_emails_by_user_and_name($user_id,$temp_name);
+        $email = $data['email'];
+        $amount = $data['amount'];
+        $times_per_day = $data['times_per_day'];
+        $commission = $data['commission'];
+        if($emailTemplate) {
+            $mailtemplate = file_get_contents(APPPATH."../assets/email_templates/$user_id/$emailTemplate->template_file");
+            $mailtemplate = str_replace('[amount]', $amount, $mailtemplate);
+            $mailtemplate = str_replace('[times_per_day]', $times_per_day, $mailtemplate);
+            $mailtemplate = str_replace('[commission]', $commission, $mailtemplate);
+            $subject = 'Your tiqs calculation';
+            $this->sendEmail($email, $subject, $mailtemplate);
+        }
+        
+        
+    }
+
+    public function sendEmail($email, $subject, $message)
+    {
+        $configemail = array(
+            'protocol' => PROTOCOL,
+            'smtp_host' => SMTP_HOST,
+            'smtp_port' => SMTP_PORT,
+            'smtp_user' => SMTP_USER,
+            'smtp_pass' => SMTP_PASS,
+            'mailtype' => 'html',
+            'charset' => 'iso-8859-1',
+            'smtp_crypto' => 'tls',
+            'wordwrap' => TRUE,
+            'newline' => "\r\n"
+        );
+
+        $config = $configemail;
+        $CI =& get_instance();
+        $CI->load->library('email', $config);
+        $CI->email->set_header('X-SES-CONFIGURATION-SET', 'ConfigSet');
+        $CI->email->set_newline("\r\n");
+        $CI->email->from('support@tiqs.com');
+        $CI->email->to($email);
+        $CI->email->subject($subject);
+        $CI->email->message($message);
+        return $CI->email->send();
     }
 
 }
