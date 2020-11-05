07b289c6a37fed046f0e2628f6e40dcb2d3060d0
aktwork <larastar721@gmail.com>
2020-11-05 11:40:15 +0100

    use validateNumber method

--- application/models/Shopprinters_model.php
+++ application/models/Shopprinters_model.php
@@ -23,7 +23,7 @@
         protected function setValueType(string $property,  &$value): void
         {
             $this->load->helper('validate_data_helper');
-            if (!Validate_data_helper::validateInteger($value)) return;
+            if (!Validate_data_helper::validateNumber($value)) return;
 
             if ($property === 'id' || $property === 'userId' || $property === 'numberOfCopies') {
                 $value = intval($value);
