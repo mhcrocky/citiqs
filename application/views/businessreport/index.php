db8528ea5e6023e80c4936da915a38c9c4323353
aktwork <larastar721@gmail.com>
2020-11-13 11:17:23 +0100

    made a small change

--- application/views/businessreport/index.php
+++ application/views/businessreport/index.php
@@ -28,7 +28,7 @@
                                         <p>TODAY</p>
                                     </div>
                                     <div class="d-flex justify-content-between pb-2">
-                                        <h2>>€ <?php echo intval($total*100)/100; ?></h2>
+                                        <h2>€ <?php echo intval($total*100)/100; ?></h2>
                                         <span></span>
                                     </div>
                                 </div>
