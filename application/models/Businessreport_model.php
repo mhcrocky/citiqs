e30aeeda2f60267cfb3ba3969453eff0add1922f
aktwork <larastar721@gmail.com>
2020-11-13 12:34:11 +0100

    added last week totals function

--- application/models/Businessreport_model.php
+++ application/models/Businessreport_model.php
@@ -64,7 +64,7 @@ WHERE vendor.id = '$vendor_id' AND tbl_shop_orders.paid = '1' AND serviceTypeId 
 	}
 
 
-	public function get_types_total($vendor_id, $order_type)
+	public function day_total($vendor_id, $order_type)
 	{
 		$date = date('Y-m-d');
 		$query = $this->db->query("SELECT tbl_shop_spot_types.type, tbl_shop_products_extended.price * tbl_shop_order_extended.quantity AS AMOUNT,
@@ -81,37 +81,99 @@ WHERE vendor.id = ".$vendor_id." AND tbl_shop_orders.paid = '1' AND tbl_shop_spo
 		return $query->result_array();
 	}
 
-	public function get_local_total($vendor_id){
-		$results = $this->get_types_total($vendor_id, 'local');
-		$total = 0;
-		foreach($results as $result){
-			$total = $total + floatval($result['AMOUNT']);
-		}
-		return $total;
+	public function last_week_total($vendor_id, $order_type)
+	{
+		$date = date('Y-m-d');
+		$query = $this->db->query("SELECT tbl_shop_spot_types.type, tbl_shop_products_extended.price * tbl_shop_order_extended.quantity AS AMOUNT,
+tbl_shop_products_extended.deliveryPrice * tbl_shop_order_extended.quantity AS deliveryAMOUNT,tbl_shop_products_extended.pickupPrice * tbl_shop_order_extended.quantity AS pickupAMOUNT
+FROM tbl_shop_orders INNER JOIN tbl_shop_order_extended ON tbl_shop_orders.id = tbl_shop_order_extended.orderId 
+INNER JOIN tbl_shop_products_extended ON tbl_shop_order_extended.productsExtendedId = tbl_shop_products_extended.id
+INNER JOIN tbl_shop_products ON  tbl_shop_products_extended.productId = tbl_shop_products.id
+INNER JOIN tbl_shop_categories ON  tbl_shop_products.categoryId = tbl_shop_categories.id 
+INNER JOIN (SELECT * FROM tbl_user WHERE roleid = 2) AS vendor ON vendor.id = tbl_shop_categories.userId
+INNER JOIN (SELECT * FROM tbl_user  WHERE roleid = 6 OR roleid = 2) AS buyer ON buyer.id = tbl_shop_orders.buyerId
+INNER JOIN tbl_shop_spots ON tbl_shop_orders.spotId = tbl_shop_spots.id
+INNER JOIN tbl_shop_spot_types ON tbl_shop_orders.serviceTypeId = tbl_shop_spot_types.id
+WHERE vendor.id = ".$vendor_id." AND tbl_shop_orders.paid = '1' AND tbl_shop_spot_types.type = '$order_type' AND updated > DATE_SUB(now(), INTERVAL 7 DAY)");
+		return $query->result_array();
 	}
 
-	public function get_delivery_total($vendor_id){
-		$results = $this->get_types_total($vendor_id, 'delivery');
-		$total = 0;
-		foreach($results as $result){
-			$total = $total + floatval($result['deliveryAMOUNT']);
+	public function get_day_totals($vendor_id){
+		$local_results = $this->day_total($vendor_id, 'local');
+		$delivery_results = $this->day_total($vendor_id, 'delivery');
+		$pickup_results = $this->day_total($vendor_id, 'pickup');
+		$local_total = 0;
+		$delivery_total = 0;
+		$pickup_total = 0;
+		foreach($local_results as $local_result){
+			$local_total = $local_total + floatval($local_result['AMOUNT']);
 		}
-		return $total;
+		foreach($delivery_results as $delivery_result){
+			$delivery_total = $delivery_total + floatval($delivery_result['deliveryAMOUNT']);
+		}
+		foreach($pickup_results as $pickup_result){
+			$pickup_total = $pickup_total + floatval($pickup_result['pickupAMOUNT']);
+		}
+		$total = $local_total + $delivery_total + $pickup_total;
+		$totals = array(
+			'total' => $total,
+			'local' => $local_total,
+			'delivery' => $delivery_total,
+			'pickup' => $pickup_total
+		);
+		return $totals;
 	}
 
-	public function get_pickup_total($vendor_id){
-		$results = $this->get_types_total($vendor_id, 'pickup');
-		$total = 0;
-		foreach($results as $result){
-			$total = $total + floatval($result['pickupAMOUNT']);
+	public function get_last_week_totals($vendor_id){
+		$local_results = $this->last_week_total($vendor_id, 'local');
+		$delivery_results = $this->last_week_total($vendor_id, 'delivery');
+		$pickup_results = $this->last_week_total($vendor_id, 'pickup');
+		$local_total = 0;
+		$delivery_total = 0;
+		$pickup_total = 0;
+		foreach($local_results as $local_result){
+			$local_total = $local_total + floatval($local_result['AMOUNT']);
 		}
-		return $total;
+		foreach($delivery_results as $delivery_result){
+			$delivery_total = $delivery_total + floatval($delivery_result['deliveryAMOUNT']);
+		}
+		foreach($pickup_results as $pickup_result){
+			$pickup_total = $pickup_total + floatval($pickup_result['pickupAMOUNT']);
+		}
+		$total = $local_total + $delivery_total + $pickup_total;
+		$totals = array(
+			'total' => $total,
+			'local' => $local_total,
+			'delivery' => $delivery_total,
+			'pickup' => $pickup_total
+		);
+		return $totals;
 	}
 
 	public function get_service_types(){
 		$query = $this->db->get('tbl_shop_spot_types');
 		return $query->result_array();
 	}
+
+	function sortWidgets($userId){
+        $sort = $this->input->post('sort');
+        $sortExists = $this->db->select('*')->from('tbl_widgets')->where('user_id',$userId)->get();
+        if($sortExists->num_rows() > 0){
+            $this->db->set('sort', $sort);
+            $this->db->where('user_id', $userId);
+            return $this->db->update('tbl_widgets');
+        }
+        $data = array(
+            'sort' => $sort,
+            'user_id' => $userId
+        );
+        return $this->db->insert('tbl_widgets', $data);
+    }
+
+    function sortedWidgets($userId){
+        $query = $this->db->select('*')->from('tbl_widgets')->where('user_id',$userId)->get();
+        return $query->result_array()[0];
+    }
 
 
 }
