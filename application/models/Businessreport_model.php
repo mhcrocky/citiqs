fc7821d181a6ff87e3516b6907827ff53f4ec00a
aktwork <larastar721@gmail.com>
2020-11-15 00:35:24 +0100

    fixed queries date mistakes

--- application/models/Businessreport_model.php
+++ application/models/Businessreport_model.php
@@ -9,7 +9,7 @@ class Businessreport_model extends CI_Model {
 
 	public function get_local_report($vendor_id)
 	{
-		$query = $this->db->query("SELECT tbl_shop_orders.id AS order_id, tbl_shop_orders.updated AS order_date, tbl_shop_products_extended.vatpercentage AS productVat,tbl_shop_products_extended.`name` AS productName,tbl_shop_products_extended.price,tbl_shop_order_extended.quantity,
+		$query = $this->db->query("SELECT tbl_shop_orders.id AS order_id, tbl_shop_orders.created AS order_date, tbl_shop_products_extended.vatpercentage AS productVat,tbl_shop_products_extended.`name` AS productName,tbl_shop_products_extended.price,tbl_shop_order_extended.quantity,
 tbl_shop_spot_types.type AS service_type,tbl_shop_products_extended.price * tbl_shop_order_extended.quantity AS AMOUNT,
 ((tbl_shop_products_extended.price * tbl_shop_order_extended.quantity) * 100)/(tbl_shop_products_extended.vatpercentage+100) AS EXVAT,
 (tbl_shop_orders.serviceFee)*(tbl_shop_vendors.serviceFeeTax/100) AS EXVATSERVICE,tbl_shop_orders.serviceFee,
@@ -29,7 +29,7 @@ WHERE vendor.id = '$vendor_id' AND tbl_shop_orders.paid = '1' AND serviceTypeId 
 
 
 	public function get_delivery_report($vendor_id){
-		$query = $this->db->query("SELECT tbl_shop_orders.id AS order_id, tbl_shop_orders.updated AS order_date,tbl_shop_products_extended.deliveryVatpercentage AS productVat,tbl_shop_products_extended.`name` AS productName,tbl_shop_products_extended.deliveryPrice AS price,
+		$query = $this->db->query("SELECT tbl_shop_orders.id AS order_id, tbl_shop_orders.created AS order_date,tbl_shop_products_extended.deliveryVatpercentage AS productVat,tbl_shop_products_extended.`name` AS productName,tbl_shop_products_extended.deliveryPrice AS price,
 tbl_shop_order_extended.quantity,
 tbl_shop_spot_types.type AS service_type,tbl_shop_products_extended.deliveryPrice * tbl_shop_order_extended.quantity AS AMOUNT,
 (tbl_shop_orders.serviceFee)*(tbl_shop_vendors.serviceFeeTax/100) AS EXVATSERVICE,tbl_shop_orders.serviceFee,
@@ -50,7 +50,7 @@ WHERE vendor.id = '$vendor_id' AND tbl_shop_orders.paid = '1' AND serviceTypeId 
 
 
 	public function get_pickup_report($vendor_id){
-		$query = $this->db->query("SELECT tbl_shop_orders.id AS order_id, tbl_shop_orders.updated AS order_date,tbl_shop_products_extended.pickupVatpercentage AS productVat,tbl_shop_products_extended.`name` AS productName,tbl_shop_products_extended.pickupPrice AS price,
+		$query = $this->db->query("SELECT tbl_shop_orders.id AS order_id, tbl_shop_orders.created AS order_date,tbl_shop_products_extended.pickupVatpercentage AS productVat,tbl_shop_products_extended.`name` AS productName,tbl_shop_products_extended.pickupPrice AS price,
 tbl_shop_order_extended.quantity,
 tbl_shop_spot_types.type AS service_type,tbl_shop_products_extended.pickupPrice * tbl_shop_order_extended.quantity AS AMOUNT,
 (tbl_shop_orders.serviceFee)*(tbl_shop_vendors.serviceFeeTax/100) AS EXVATSERVICE,tbl_shop_orders.serviceFee,
@@ -83,7 +83,7 @@ INNER JOIN (SELECT * FROM tbl_user WHERE roleid = 2) AS vendor ON vendor.id = tb
 INNER JOIN (SELECT * FROM tbl_user  WHERE roleid = 6 OR roleid = 2) AS buyer ON buyer.id = tbl_shop_orders.buyerId
 INNER JOIN tbl_shop_spots ON tbl_shop_orders.spotId = tbl_shop_spots.id
 INNER JOIN tbl_shop_spot_types ON tbl_shop_orders.serviceTypeId = tbl_shop_spot_types.id
-WHERE vendor.id = ".$vendor_id." AND tbl_shop_orders.paid = '1' AND tbl_shop_spot_types.type = '$order_type' AND DATE(tbl_shop_orders.updated) = '$date' ");
+WHERE vendor.id = ".$vendor_id." AND tbl_shop_orders.paid = '1' AND tbl_shop_spot_types.type = '$order_type' AND DATE(tbl_shop_orders.created) = '$date' ");
 		return $query->result_array();
 	}
 
@@ -100,7 +100,7 @@ INNER JOIN (SELECT * FROM tbl_user WHERE roleid = 2) AS vendor ON vendor.id = tb
 INNER JOIN (SELECT * FROM tbl_user  WHERE roleid = 6 OR roleid = 2) AS buyer ON buyer.id = tbl_shop_orders.buyerId
 INNER JOIN tbl_shop_spots ON tbl_shop_orders.spotId = tbl_shop_spots.id
 INNER JOIN tbl_shop_spot_types ON tbl_shop_orders.serviceTypeId = tbl_shop_spot_types.id
-WHERE vendor.id = ".$vendor_id." AND tbl_shop_orders.paid = '1' AND tbl_shop_spot_types.type = '$order_type' AND updated > DATE_SUB(now(), INTERVAL 7 DAY)");
+WHERE vendor.id = ".$vendor_id." AND tbl_shop_orders.paid = '1' AND tbl_shop_spot_types.type = '$order_type' AND tbl_shop_orders.created > DATE_SUB(now(), INTERVAL 7 DAY)");
 		return $query->result_array();
 	}
 
@@ -117,7 +117,7 @@ INNER JOIN (SELECT * FROM tbl_user WHERE roleid = 2) AS vendor ON vendor.id = tb
 INNER JOIN (SELECT * FROM tbl_user  WHERE roleid = 6 OR roleid = 2) AS buyer ON buyer.id = tbl_shop_orders.buyerId
 INNER JOIN tbl_shop_spots ON tbl_shop_orders.spotId = tbl_shop_spots.id
 INNER JOIN tbl_shop_spot_types ON tbl_shop_orders.serviceTypeId = tbl_shop_spot_types.id
-WHERE vendor.id = ".$vendor_id." AND tbl_shop_orders.paid = '1' AND tbl_shop_spot_types.type = '$order_type' AND updated > DATE_SUB(now(), INTERVAL 14 DAY) AND updated <= DATE_SUB(now(), INTERVAL 7 DAY)");
+WHERE vendor.id = ".$vendor_id." AND tbl_shop_orders.paid = '1' AND tbl_shop_spot_types.type = '$order_type' AND tbl_shop_orders.created > DATE_SUB(now(), INTERVAL 14 DAY) AND tbl_shop_orders.created <= DATE_SUB(now(), INTERVAL 7 DAY)");
 		return $query->result_array();
 	}
 
