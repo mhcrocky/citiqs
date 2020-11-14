91bf157d21ba16d18c5c981f5d344b2349220559
aktwork <larastar721@gmail.com>
2020-11-14 17:47:32 +0100

    testing

--- application/models/Businessreport_model.php
+++ application/models/Businessreport_model.php
@@ -9,7 +9,7 @@ class Businessreport_model extends CI_Model {
 
 	public function get_local_report($vendor_id)
 	{
-		$query = $this->db->query("SELECT tbl_shop_orders.id AS order_id, tbl_shop_orders.updated AS order_date, tbl_shop_products_extended.vatpercentage AS productVat,tbl_shop_products_extended.`name` AS productName,tbl_shop_products_extended.price,tbl_shop_order_extended.quantity,
+		$query = $this->db->query("SELECT tbl_shop_orders.id AS order_id, tbl_shop_vendors.serviceFeeTax as vendor_serviceFeeTax, tbl_shop_orders.updated AS order_date, tbl_shop_products_extended.vatpercentage AS productVat,tbl_shop_products_extended.`name` AS productName,tbl_shop_products_extended.price,tbl_shop_order_extended.quantity,
 tbl_shop_spot_types.type AS service_type,tbl_shop_products_extended.price * tbl_shop_order_extended.quantity AS AMOUNT,
 ((tbl_shop_products_extended.price * tbl_shop_order_extended.quantity) * 100)/(tbl_shop_products_extended.vatpercentage+100) AS EXVAT,
 tbl_shop_products_extended.price * tbl_shop_order_extended.quantity-tbl_shop_products_extended.price * tbl_shop_order_extended.quantity * 100 / (tbl_shop_products_extended.vatpercentage+100) AS VAT
@@ -17,11 +17,12 @@ FROM tbl_shop_orders INNER JOIN tbl_shop_order_extended ON tbl_shop_orders.id = 
 INNER JOIN tbl_shop_products_extended ON tbl_shop_order_extended.productsExtendedId = tbl_shop_products_extended.id
 INNER JOIN tbl_shop_products ON  tbl_shop_products_extended.productId = tbl_shop_products.id
 INNER JOIN tbl_shop_categories ON  tbl_shop_products.categoryId = tbl_shop_categories.id 
+INNER JOIN tbl_shop_vendors ON  tbl_shop_vendors.vendorId = tbl_shop_categories.userId 
 INNER JOIN (SELECT * FROM tbl_user WHERE roleid = 2) AS vendor ON vendor.id = tbl_shop_categories.userId
 INNER JOIN (SELECT * FROM tbl_user  WHERE roleid = 6 OR roleid = 2) AS buyer ON buyer.id = tbl_shop_orders.buyerId
 INNER JOIN tbl_shop_spots ON tbl_shop_orders.spotId = tbl_shop_spots.id
 INNER JOIN tbl_shop_spot_types ON tbl_shop_orders.serviceTypeId = tbl_shop_spot_types.id
-WHERE vendor.id = ".$vendor_id." AND tbl_shop_orders.paid = '1' AND serviceTypeId = '1'");
+WHERE vendor.id = '418' AND tbl_shop_orders.paid = '1' AND serviceTypeId = '1'");
 		return $query->result_array();
 	}
 
