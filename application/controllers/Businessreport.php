2fa53521589cb39e3bc5eb6eed999f5bcecec160
aktwork <larastar721@gmail.com>
2020-11-14 23:44:15 +0100

    merged function from visma

--- application/controllers/Businessreport.php
+++ application/controllers/Businessreport.php
@@ -33,23 +33,80 @@ class Businessreport extends BaseControllerWeb
 		$delivery = $this->businessreport_model->get_delivery_report($vendor_id);
 		$local = $this->businessreport_model->get_local_report($vendor_id);
 		$report = array_merge($pickup, $delivery, $local);
+		$report = $this->group_by('order_id',$report);
+		$report = $this->table_data($report);
 		echo json_encode($report);
 		
 	}
 
 	public function sortWidgets()
     {
-        $this->load->model('user_modelpublic');
+    
         $userId = $this->session->userdata("userId");
         $this->businessreport_model->sortWidgets($userId);  
     }
 
     public function sortedWidgets()
     {
-        $this->load->model('user_modelpublic');
         $userId = $this->session->userdata("userId");
         echo json_encode($this->businessreport_model->sortedWidgets($userId));
-    }
+	}
+	
+
+
+	function group_by($key, $data) {
+
+		$result = array();
+
+		foreach($data as $val) {
+			if(array_key_exists($key, $val)){
+				$result[$val[$key]][] = $val;
+			}else{
+				$result[""][] = $val;
+			}
+		}
+
+		return $result;
+	}
+
+	function row_total($key,$data){
+		$sum = 0;
+		foreach ($data as $item) {
+			$sum += $item[$key];
+		}
+		return $sum;
+	}
+
+	function table_data($data){
+		$rows = [];
+		foreach($data as $key =>$val){
+			$total_price = $this->row_total('price',$val);
+			$total_quantity = $this->row_total('quantity',$val);
+			$total_AMOUNT = $this->row_total('AMOUNT',$val)+$val[0]['serviceFee'];
+			$total_EXVAT = $this->row_total('EXVAT',$val);
+			$total_VAT = $this->row_total('VAT',$val);
+			$total_EXVATSERVICE = $this->row_total('EXVATSERVICE',$val);
+
+			
+			
+			$rows[] = [
+				'order_id'=>$val[0]['order_id'],
+				'order_date'=>$val[0]['order_date'],
+				'serviceFee'=>$val[0]['serviceFee'],
+				'service_type'=>$val[0]['service_type'],
+				'price'=>$total_price,
+				'quantity'=>$total_quantity,
+				'AMOUNT'=>$total_AMOUNT,
+				'EXVAT'=>$total_EXVAT,
+				'VAT'=>$total_VAT,
+				'EXVATSERVICE' => $total_EXVATSERVICE,
+				'child'=>$val];
+		}
+
+		return $rows;
+
+	}
+
 
 
 }
