8670fe0adee3ba3e831caf2025c50ea7127ee698
aktwork <larastar721@gmail.com>
2020-11-15 21:21:03 +0100

    added get_date_range_totals function

--- application/models/Businessreport_model.php
+++ application/models/Businessreport_model.php
@@ -124,6 +124,51 @@ WHERE vendor.id = ".$vendor_id." AND tbl_shop_orders.paid = '1' AND tbl_shop_spo
 		return $query->result_array();
 	}
 
+
+	public function date_range_total($vendor_id, $min_date, $max_date, $order_type)
+	{
+		$query = $this->db->query("SELECT tbl_shop_spot_types.type, tbl_shop_products_extended.price * tbl_shop_order_extended.quantity AS AMOUNT,
+tbl_shop_products_extended.deliveryPrice * tbl_shop_order_extended.quantity AS deliveryAMOUNT,tbl_shop_products_extended.pickupPrice * tbl_shop_order_extended.quantity AS pickupAMOUNT
+FROM tbl_shop_orders INNER JOIN tbl_shop_order_extended ON tbl_shop_orders.id = tbl_shop_order_extended.orderId 
+INNER JOIN tbl_shop_products_extended ON tbl_shop_order_extended.productsExtendedId = tbl_shop_products_extended.id
+INNER JOIN tbl_shop_products ON  tbl_shop_products_extended.productId = tbl_shop_products.id
+INNER JOIN tbl_shop_categories ON  tbl_shop_products.categoryId = tbl_shop_categories.id 
+INNER JOIN (SELECT * FROM tbl_user WHERE roleid = 2) AS vendor ON vendor.id = tbl_shop_categories.userId
+INNER JOIN (SELECT * FROM tbl_user  WHERE roleid = 6 OR roleid = 2) AS buyer ON buyer.id = tbl_shop_orders.buyerId
+INNER JOIN tbl_shop_spots ON tbl_shop_orders.spotId = tbl_shop_spots.id
+INNER JOIN tbl_shop_spot_types ON tbl_shop_orders.serviceTypeId = tbl_shop_spot_types.id
+WHERE vendor.id = ".$vendor_id." AND tbl_shop_orders.paid = '1' AND tbl_shop_spot_types.type = '$order_type' 
+AND tbl_shop_orders.created >= $min_date AND tbl_shop_orders.created <= $max_date");
+		return $query->result_array();
+	}
+
+
+	public function get_date_range_totals($vendor_id, $min_date, $max_date){
+		$local_results = $this->date_range_total($vendor_id, $min_date, $max_date, 'local');
+		$delivery_results = $this->date_range_total($vendor_id, $min_date, $max_date, 'delivery');
+		$pickup_results = $this->date_range_total($vendor_id, $min_date, $max_date, 'pickup');
+		$local_total = 0;
+		$delivery_total = 0;
+		$pickup_total = 0;
+		foreach($local_results as $local_result){
+			$local_total = $local_total + floatval($local_result['AMOUNT']);
+		}
+		foreach($delivery_results as $delivery_result){
+			$delivery_total = $delivery_total + floatval($delivery_result['deliveryAMOUNT']);
+		}
+		foreach($pickup_results as $pickup_result){
+			$pickup_total = $pickup_total + floatval($pickup_result['pickupAMOUNT']);
+		}
+		$total = $local_total + $delivery_total + $pickup_total;
+		$totals = array(
+			'total' => $total,
+			'local' => $local_total,
+			'delivery' => $delivery_total,
+			'pickup' => $pickup_total
+		);
+		return $totals;
+	}
+
 	public function get_day_totals($vendor_id){
 		$local_results = $this->day_total($vendor_id, 'local');
 		$delivery_results = $this->day_total($vendor_id, 'delivery');
