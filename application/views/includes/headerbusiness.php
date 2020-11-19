b8b2bda3543ada09c2e748aa1b4665276cc89f67
aktwork <larastar721@gmail.com>
2020-11-19 22:26:34 +0100

    added agenda in menu

--- application/views/includes/headerbusiness.php
+++ application/views/includes/headerbusiness.php
@@ -145,6 +145,7 @@
                     <li><a href="<?php echo $this->baseUrl; ?>orders"><i class="ti-stats-up"></i> <span>Orders</span></a></li>
                     <li><a href="<?php echo $this->baseUrl; ?>pos"><i class="ti-bar-chart"></i> <span>POS</span></a></li>
                     <li><a href="<?php echo $this->baseUrl;?>dashboard"><i class="ti-receipt"></i> <span>Business Report</span></a></li>
+                    <li><a href="<?php echo $this->baseUrl;?>customer_panel/agenda"><i class="ti-agenda"></i> <span>Agenda</span></a></li>
                     <li>
                         <a href="javascript:void(0)" aria-expanded="true"><i class="ti-shopping-cart-full"></i><span>Marketing</span></a>
                         <ul class="collapse">
