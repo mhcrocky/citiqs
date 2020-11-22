8c38b72d69f1ce0380cd09b1de984009ed86aece
aktwork <larastar721@gmail.com>
2020-11-22 19:29:31 +0100

    modified products order by category

--- application/views/warehouse/productsOrder.php
+++ application/views/warehouse/productsOrder.php
@@ -1,6 +1,24 @@
 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/rowreorder/1.2.7/css/rowReorder.dataTables.min.css">
+<style>
+.custom-select {
+  width: 65px !important;
+}
+</style>
 
+<div style="padding:25px;" class="w-100 mt-3 row-sort ui-sortable" data-rowposition="2" data-rowsort="1">
+<div class="w-100 mt-3 mb-3 mx-auto">
+
+  <div class="float-right text-center">
+  <label for="category">Choose Category</label>
+    <select style="width: 264px  !important;font-size: 14px;" class="custom-select custom-select-sm form-control form-control-sm  mb-1 " id="category">
+       
+        <?php foreach ($categories as $category): ?>
+        <option  value="<?php echo strtolower($category['category']); ?>"><?php echo ucfirst($category['category']); ?></option>
+        <?php endforeach;?>
+    </select>
+    </div>
+</div>
 
 <div class="w-100 mt-3 p-5 table-responsive">
   <table id="products" class="table table-striped table-bordered" cellspacing="0" width="100%">
@@ -16,6 +34,7 @@
 
 
   $(document).ready( function () {
+    $("select option:nth-child(1)").attr("selected", true);
       var table = $('#products').DataTable({
         processing: true,
         colReorder: true,
@@ -24,8 +43,8 @@
         deferRender: true,
         scroller: true,
         ordering: true,
-        lengthMenu: [[5, 10, 20, 50, 100, 200, 500, -1], [5, 10, 20, 50, 100, 200, 500, 'All']],
-        pageLength: 5,
+        lengthMenu: [[200, 500, -1], [200, 500, 'All']],
+        pageLength: 200,
         ajax: {
           type: 'get',
           url: '<?php echo base_url("getproducts"); ?>',
@@ -57,22 +76,39 @@
         }
         ],
       });
+
+      var category = $('#category').val();
+        table
+        .columns( 3 )
+        .search( category )
+        .draw();
+
+      $('#category').change(function() {
+        var category = this.value;
+        table
+        .columns( 3 )
+        .search( category )
+        .draw();
+      });
       
       table.on( 'row-reordered', function ( e, diff, edit ) {
         var products = [];
+        var i = 1;
         table.one( 'draw', function () {
           console.log( 'Redraw occurred at: '+new Date().getTime() );
-          table.rows().every(function (rowIdx, tableLoop, rowLoop) {
+          table.rows({search:'applied'}).every(function (rowIdx, tableLoop, rowLoop) {
             console.log(rowIdx, this.data());
-            //products.push(array(this.data().id,this.data().position));
+            products.push([this.data().id,i]);
             var data = {
               productId: this.data().id,
               orderNo: this.data().position
             }
-            $.post('<?php echo base_url("updateproductorderno") ?>',data)
+            i++;
+            //$.post('<?php echo base_url("updateproductorderno") ?>',data)
           });
           if (products.length > 0) {
           console.log(products);
+          $.post('<?php echo base_url("updateproductorderno") ?>',{products:products});
         }
         });
 
