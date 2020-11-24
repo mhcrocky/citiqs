5492a6510a2184cbd692ed48869cf404bb7bc53d
aktwork <larastar721@gmail.com>
2020-11-24 22:10:17 +0100

    added redirect to select payment type on price equal with zero

--- application/controllers/Booking_agenda.php
+++ application/controllers/Booking_agenda.php
@@ -254,6 +254,10 @@ class Booking_agenda extends BaseControllerWeb
                 $this->session->set_userdata('selectedTimeSlot', $selectedTimeSlot);
             }
 
+            if($spot->price == 0){
+                redirect('booking_agenda/select_payment_type');
+            }
+
             if(AVAILABLE_TO_BOOK_EXTRA_TIME == true && HOW_MANY_SLOTS_CAN_BE_BOOKED > 1){
                 redirect('booking_agenda/reserved');
             } else {
