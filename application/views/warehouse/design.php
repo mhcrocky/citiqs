10078c93d045dddef0e26d3505ade2d13028ac68
aktwork <larastar721@gmail.com>
2020-11-29 19:21:03 +0100

    fixed design issue

--- application/views/warehouse/design.php
+++ application/views/warehouse/design.php
@@ -1,4 +1,22 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/home/styles/design.css">
+<style>
+    .form-control {
+    display: block;
+    width: 100%;
+    height: calc(1.5em + .75rem + 2px);
+    padding: .375rem .75rem;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #495057;
+    background-color: #fff;
+    background-clip: padding-box;
+    border: 1px solid #ced4da;
+    border-radius: .25rem;
+    transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+}
+</style>
 <script src="<?php echo base_url(); ?>assets/home/js/design.js"></script>
 <main class="w-100 container" style="margin-top:20px">
 
