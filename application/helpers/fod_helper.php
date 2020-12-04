d2bac9575ab9db0b8cb8bf10ad91c2d1376c612e
aktwork <larastar721@gmail.com>
2020-12-04 09:39:02 +0100

    activate isFodActive method

--- application/helpers/fod_helper.php
+++ application/helpers/fod_helper.php
@@ -8,7 +8,7 @@
 
         public static function isFodActive(int $venodrId, int $spotId): bool
         {
-            return true;
+            // return true;
             $CI =& get_instance();
             $CI->load->model('shopvendorfod_model');
 
