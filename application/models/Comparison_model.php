f17e354f2cccb17a267db84cb41a84bfb9bab387
aktwork <larastar721@gmail.com>
2020-12-12 23:47:32 +0100

    modified the query

--- application/models/Comparison_model.php
+++ application/models/Comparison_model.php
@@ -9,20 +9,21 @@ class Comparison_model extends CI_Model {
 	public function getOrderId($vendorId)
 	{
 
-		$this->db->select('tbl_shop_orders.id');
+		$this->db->select('tbl_shop_orders.old_order');
         $this->db->from('tbl_shop_orders');
         $this->db->join('tbl_shop_order_extended', 'tbl_shop_order_extended.orderId = tbl_shop_orders.id', 'left');
 		$this->db->join('tbl_shop_products_extended', 'tbl_shop_products_extended.id = tbl_shop_order_extended.productsExtendedId', 'left');
 		$this->db->join('tbl_shop_products', 'tbl_shop_products.id = tbl_shop_products_extended.productId', 'left');
         $this->db->join('tbl_shop_categories', 'tbl_shop_categories.id = tbl_shop_products.categoryId', 'left');
-        $this->db->where('tbl_shop_categories.userId', $vendorId);
+		$this->db->where('tbl_shop_categories.userId', $vendorId);
+		$this->db->group_by('tbl_shop_orders.old_order');
 
 		$query = $this->db->get();
         if ($query) {
 			$results = $query->result_array();
 			$orderIds = [];
 			foreach($results as $result){
-				$orderIds[] = $result['id'];
+				$orderIds[] = $result['old_order'];
 			}
 			return $orderIds;
         }
