86172bcc5fe14c18c7defca10dc7c9b947f248b8
aktwork <larastar721@gmail.com>
2020-12-27 15:51:58 +0100

    made a small change

--- application/views/new_bookings/index.php
+++ application/views/new_bookings/index.php
@@ -1,9 +1,4 @@
-<style>
-#date-active {
-    background: #b32400 !important;
-}
-</style>
-		<div class="row">
+        <div class="row">
 			<form action="" class="w-100">
 				<div class="col-12 step step-1 active">
 					<h3 id="title">Choose a Date</h3>
