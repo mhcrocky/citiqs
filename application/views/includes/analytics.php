fd62f459f9560755250554fd4d8c6c58aff6522d
aktwork <larastar721@gmail.com>
2020-12-31 18:33:56 +0100

    Analytics conversion lanel

--- application/views/includes/analytics.php
+++ application/views/includes/analytics.php
@@ -13,6 +13,18 @@
 <?php } ?>
 <!-- End Google Analytics -->
 
+<!-- Start Google Tag Manager Code -->
+<?php if ($vendor['googleTagManagerCode']) { ?>
+    <script>
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','<?php echo $vendor['googleTagManagerCode']; ?>');
+    </script>
+<?php } ?>
+<!-- End Google Tag Manager Code -->
+
 <!-- Start Google Adwords Conversion Id -->
 <?php if ($vendor['googleAdwordsConversionId']) { ?>
     <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $vendor['googleAdwordsConversionId']; ?>"></script>
@@ -26,23 +38,16 @@
 <!-- End Google Adwords Conversion Id -->
 
 <!-- Start Google Adwords Conversion Label -->
-<?php if ($vendor['googleAdwordsConversionLabel']) { ?>
-
+<?php if ($vendor['googleAdwordsConversionLabel'] && $vendor['googleAdwordsConversionId']) { ?>
+    <script>
+        gtag('event', 'conversion', {'send_to': '<?php echo $vendor['googleAdwordsConversionId']; ?>/<?php echo $vendor['googleAdwordsConversionLabel']; ?>',
+            'value': 1.0,
+            'currency': 'EUR'
+        });
+    </script>
 <?php } ?>
 <!-- End Google Adwords Conversion Label -->
 
-
-<!-- Start Google Tag Manager Code -->
-<?php if ($vendor['googleTagManagerCode']) { ?>
-    <script>
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','<?php echo $vendor['googleTagManagerCode']; ?>');
-    </script>
-<?php } ?>
-<!-- End Google Tag Manager Code -->
 
 <!-- Start Facebook Pixed Id -->
 <?php if ($vendor['facebookPixelId']) { ?>
