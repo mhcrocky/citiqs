9525fcb512c705d6d4ddf0034d5b09283b699c01
aktwork <larastar721@gmail.com>
2021-01-05 02:22:48 +0100

    first commit

---
+++ assets/home/js/eventDataTables.js
@@ -0,0 +1,233 @@
+$(document).ready(function() {
+    var groupColumn = 2;
+    var table = $('#tickets').DataTable({
+        columnDefs: [{
+            "visible": false,
+            "targets": groupColumn
+        }],
+        ajax: {
+            type: 'get',
+            url: globalVariables.baseUrl +"events/test",
+            dataSrc: '',
+        },
+        columns: [{
+
+                title: 'Design',
+                data: null,
+                "render": function(data, type, row) {
+                    return "<div class='bg-primary' style='width: 30px;height: 30px;font-size: 16px;'></div>";
+                }
+
+            },
+
+            {
+
+                title: 'Ticket Name',
+                data: null,
+                "render": function(data, type, row) {
+                    return '<input type="text" id="event-name" class="form-control" name="event-name" value="' +
+                        data.name + '">';
+                }
+
+            },
+            {
+
+                title: 'Group',
+                data: 'group'
+
+            },
+            {
+
+                title: 'Quantity',
+                data: null,
+                "render": function(data, type, row) {
+                    return '<input type="text" id="quantity" class="form-control" name="quantity" value="' +
+                        data.quantity + '">';
+                }
+
+            },
+
+            {
+
+                title: 'Price (Inc. VAT)',
+                data: null,
+                "render": function(data, type, row) {
+                    return '<input type="text" id="price" class="form-control" name="price" value="' +
+                        data.price + '">';
+                }
+
+            },
+            {
+
+                title: 'Visible',
+                data: null,
+                "render": function(data, type, row) {
+                    return '<ul><li><div class="custom-control custom-checkbox"><input style="transform: scale(1.5);" class="custom-control-input" id="package-area-' +
+                        data.id +
+                        '"  type="checkbox" checked="checked" ><label class="custom-control-label" for="package-area-' +
+                        data.id + '"> </label>  </div>    </li></ul>';
+                }
+
+            },
+            {
+
+                title: 'Options',
+                data: null,
+                "render": function(data, type, row) {
+                    return "<div class='bg-success' style='width: 30px;height: 30px;'><i style='color: #fff;' class='fa fa-pencil p-2'><i></div>";
+                }
+
+            },
+            {
+
+                title: '',
+                data: null,
+                "render": function(data, type, row) {
+                    return "<div class='bg-dark' style='width: 30px;height: 30px;'><i style='color: #fff;' class='fa fa-trash p-2'><i></div>";
+                }
+
+            }
+        ],
+        order: [
+            [groupColumn, 'asc']
+        ],
+        displayLength: 25,
+        drawCallback: function(settings) {
+            $("#tickets_filter").remove();
+            $("#tickets_length").remove();
+
+            var api = this.api();
+            var rows = api.rows({
+                page: 'current'
+            }).nodes();
+            var last = null;
+
+            api.column(groupColumn, {
+                page: 'current'
+            }).data().each(function(group, i) {
+                if (last !== group) {
+                    if (group == '') {
+
+                    } else {
+                        $(rows).eq(i).before(
+                            '<tr class="group"><td><div class="bg-primary" style="background: #00264d!important;width: 30px;height: 30px;"></div></td><td colspan="3"><input type="text" id="event-name" class="form-control" name="event-name" value="' +
+                            group + '">' +
+                            '</td><td><ul><li><div class="custom-control custom-checkbox"><input style="transform: scale(1.5);" class="custom-control-input" id="package-area-0' +
+                            i +
+                            '" type="checkbox" checked="checked" ><label class="custom-control-label" for="package-area-0' +
+                            i + '"> </label>  </div>    </li></ul></td><td></td>' +
+                            '<td><div class="bg-dark" style="width: 30px;height: 30px;"><i style="color: #fff;" class="fa fa-trash p-2"><i></div></td></tr>'
+                        );
+                    }
+
+                    last = group;
+                }
+            });
+        }
+    });
+
+
+    var table2 = $('#additional-item').DataTable({
+        columnDefs: [{
+            "visible": false,
+            "targets": groupColumn
+        }],
+        ajax: {
+            type: 'get',
+            url: globalVariables.baseUrl + "events/test",
+            dataSrc: '',
+        },
+        columns: [{
+
+                title: 'Design',
+                data: null,
+                "render": function(data, type, row) {
+                    return "<div class='bg-warning' style='width: 30px;height: 30px;'></div>";
+                }
+
+            },
+
+            {
+
+                title: 'Ticket Name',
+                data: null,
+                "render": function(data, type, row) {
+                    return '<input type="text" id="event-name" class="form-control" name="event-name" value="' +
+                        data.name + '">';
+                }
+
+            },
+            {
+
+                title: 'Quantity',
+                data: null,
+                "render": function(data, type, row) {
+                    return '<input type="text" id="quantity" class="form-control" name="quantity" value="' +
+                        data.quantity + '">';
+                }
+
+            },
+
+            {
+
+                title: 'Price (Inc. VAT)',
+                data: null,
+                "render": function(data, type, row) {
+                    return '<input type="text" id="price" class="form-control" name="price" value="' +
+                        data.price + '">';
+                }
+
+            },
+            {
+
+                title: 'Visible',
+                data: null,
+                "render": function(data, type, row) {
+                    return '<ul><li><div class="custom-control custom-checkbox"><input style="transform: scale(1.5);" class="custom-control-input" id="package-area-' +
+                        data.id +
+                        '"  type="checkbox" checked="checked" ><label class="custom-control-label" for="package-area-' +
+                        data.id + '"> </label>  </div>    </li></ul>';
+                }
+
+            },
+            {
+
+                title: 'Options',
+                data: null,
+                "render": function(data, type, row) {
+                    return "<div class='bg-success' style='width: 30px;height: 30px;'><i style='color: #fff;' class='fa fa-pencil p-2'><i></div>";
+                }
+
+            },
+            {
+
+                title: '',
+                data: null,
+                "render": function(data, type, row) {
+                    return "<div class='bg-dark' style='width: 30px;height: 30px;'><i style='color: #fff;' class='fa fa-trash p-2'><i></div>";
+                }
+
+            }
+        ],
+        order: [
+            [groupColumn, 'asc']
+        ],
+        displayLength: 25,
+        drawCallback: function(settings) {
+            $("#additional-item_filter").remove();
+            $("#additional-item_length").remove();
+        }
+    });
+
+
+    /*Order by the grouping
+    $('#tickets tbody').on('click', 'tr.group', function() {
+        var currentOrder = table.order()[0];
+        if (currentOrder[0] === groupColumn && currentOrder[1] === 'asc') {
+            table.order([groupColumn, 'desc']).draw();
+        } else {
+            table.order([groupColumn, 'asc']).draw();
+        }
+    });
+    */
+});
