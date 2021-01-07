355c0588b62078fd79600ef1a2e648e18bc4b3f5
aktwork <larastar721@gmail.com>
2021-01-08 00:37:34 +0100

    added ticket options functions

--- application/models/Event_model.php
+++ application/models/Event_model.php
@@ -17,6 +17,30 @@ class Event_model extends CI_Model {
 		return $this->db->insert('tbl_event_tickets',$data);
 	}
 
+	public function save_ticket_options($data)
+	{
+		$ticketId = $data['ticketId'];
+		if($ticketId == ""){
+			return $this->db->insert('tbl_ticket_options',$data);
+		}
+		$this->db->where('ticketId ',$ticketId );
+		return $this->db->update('tbl_ticket_options',$data);
+		
+	}
+
+	public function get_ticket_options($ticketId)
+	{
+		$this->db->select('*');
+		$this->db->from('tbl_ticket_options');
+		$this->db->where('ticketId', $ticketId);
+		$query = $this->db->get();
+		$results = $query->result_array();
+		if(isset($results[0])){
+			return $results[0];
+		}
+		return '';
+	}
+
 	public function get_events($vendor_id)
 	{
 		$this->db->select('id, eventname');
@@ -28,7 +52,7 @@ class Event_model extends CI_Model {
 
 	public function get_tickets($vendor_id)
 	{
-		$this->db->select('*');
+		$this->db->select('*,tbl_event_tickets.id as ticketId');
 		$this->db->from('tbl_event_tickets');
 		$this->db->join('tbl_events', 'tbl_events.id = tbl_event_tickets.eventId');
 		$this->db->where('vendorId', $vendor_id);
