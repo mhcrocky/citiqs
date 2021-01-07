995026ce4a4fd34915281c42e2336f97ef9d3242
aktwork <larastar721@gmail.com>
2021-01-07 01:43:32 +0100

    added save ticket function

--- application/models/Event_model.php
+++ application/models/Event_model.php
@@ -11,4 +11,28 @@ class Event_model extends CI_Model {
 	{
 		return $this->db->insert('tbl_events',$data);
 	}
+
+	public function save_ticket($data)
+	{
+		return $this->db->insert('tbl_event_tickets',$data);
+	}
+
+	public function get_events($vendor_id)
+	{
+		$this->db->select('id, eventname');
+		$this->db->from('tbl_events');
+		$this->db->where('vendorId', $vendor_id);
+		$query = $this->db->get();
+		return $query->result_array();
+	}
+
+	public function get_tickets($vendor_id)
+	{
+		$this->db->select('*');
+		$this->db->from('tbl_event_tickets');
+		$this->db->join('tbl_events', 'tbl_events.id = tbl_event_tickets.eventId');
+		$this->db->where('vendorId', $vendor_id);
+		$query = $this->db->get();
+		return $query->result_array();
+	}
 }
