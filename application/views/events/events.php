62a805efd67d3b2b1244940fc67c8ffa06360255
aktwork <larastar721@gmail.com>
2021-01-08 19:55:48 +0100

    first commit

---
+++ application/views/events/events.php
@@ -0,0 +1,21 @@
+
+
+
+
+
+<main class="my-form">
+    <div class="w-100 mt-5 p-3">
+    <div class="input-group col-md-2">
+    <a href="<?php echo base_url(); ?>events/create">
+            <input type="button" value="Add Event" style="background: #009933 !important;border-radius:0"
+                class="btn btn-success form-control mb-3 text-left">
+            <a style="background: #004d1a;padding-top: 8px;" class="input-group-addon pl-2 pr-2 mb-3"
+               >
+                <i style="color: #fff;font-size: 18px;" class="fa fa-plus"></i></a>
+                </a>
+        </div>
+        <table id="events" class="mt-5" style="width:100%;background: #fff;">
+
+        </table>
+    </div>
+</main>
