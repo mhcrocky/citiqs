d019cd2b1d319ba61bfae7a682573cb332e76421
aktwork <larastar721@gmail.com>
2021-01-12 00:24:11 +0100

    added style for event edit

--- application/views/includes/customCss.php
+++ application/views/includes/customCss.php
@@ -158,7 +158,7 @@
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/home/styles/blackbox.css">
 <?php } elseif ($this->view === 'publicorders/temporarilyClosed') { ?>
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/home/styles/temporarilyClosed.css">
-<?php  } elseif ($this->view === 'events/step-one') { ?>
+<?php  } elseif ($this->view === 'events/step-one' || $this->view === 'events/edit_event') { ?>
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
     <link href="<?php echo base_url(); ?>assets/css/main.jbox.min.css" rel="stylesheet" type="text/css">
     <link href="<?php echo base_url();?>assets/css/quill.snow.css" rel="stylesheet">
