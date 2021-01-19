40410cff70a0f38ad6aeb9858f2a5306d122c723
aktwork <larastar721@gmail.com>
2021-01-20 00:47:13 +0100

    modified design

--- application/views/events/tickets.php
+++ application/views/events/tickets.php
@@ -1,4 +1,3 @@
-
 <div style="background:red;text-align: center;" id="header-img" class="w-100" style="text-align:center">
 
 
@@ -7,121 +6,80 @@
     </div>
 
 </div>
-<form action="<?php echo base_url(); ?>events/your_tickets" method="POST">
-<?php if (!empty($tickets)) : ?>
+<form id="my-form" action="<?php echo base_url(); ?>events/your_tickets" method="POST">
+    <?php if (!empty($tickets)) : ?>
     <input type="hidden" id="current_time" name="current_time" value="">
-<div class="shop__item-list selectedSpotBackground full-height">
-    <?php foreach ($tickets as $ticket): ?>
-    <input type="hidden" id="quantity_<?php echo $ticket['ticketId']; ?>" name="quantity[]" value="0">
-    <input type="hidden" name="id[]" value="<?php echo $ticket['ticketId']; ?>">
-    <input type="hidden" name="descript[]" value="<?php echo $ticket['ticketDescription']; ?>">
-    <input type="hidden" name="price[]" value="<?php echo $ticket['ticketPrice']; ?>">
-    <div class="shop__single-item">
-        <div class="shop__single-item__info">
-            <!-- wrapped long description and title -->
-            <div>
-                <strong
-                    class="shop__single-item__info--title productName"><?php echo $ticket['ticketDescription']; ?></strong>
-            </div>
-        </div>
-        <div class="shop__single-item__image">
-            <img style="display: none;" src="https://tiqs.com/alfred/assets/images/productImages/2909_1608623878.png"
-                alt="<?php echo $ticket['ticketDescription']; ?>">
-        </div>
-        <!-- ADDED DIV FOR + PRICE - -->
-        <div style="min-width: 270px;" class="shop__single-item__cart-wrapper">
-            <div class="shop__single-item__price items priceQuantity">
-                <span><?php echo $ticket['ticketPrice']; ?></span>
-            </div>
-            <div class="shop__single-item__quanitity-buttons">
-                <div class="shop__single-item__add-to-cart items priceQuantity"
-                    onclick="removeOrder('<?php echo $ticket['ticketId']; ?>', '<?php echo $ticket['ticketPrice']; ?>')">
-                    <span style="font-size:16px; vertical-align: middle; text-align:center">
-                        <i class="fa fa-minus priceQuantity" aria-hidden="true"></i>
-                    </span>
+    <div class="shop__item-list selectedSpotBackground full-height">
+        <?php foreach ($tickets as $ticket): ?>
+        <input type="hidden" id="quantity_<?php echo $ticket['ticketId']; ?>" name="quantity[]" value="0">
+        <input type="hidden" name="id[]" value="<?php echo $ticket['ticketId']; ?>">
+        <input type="hidden" name="descript[]" value="<?php echo $ticket['ticketDescription']; ?>">
+        <input type="hidden" name="price[]" value="<?php echo $ticket['ticketPrice']; ?>">
+        <div class="shop__single-item">
+            <div class="shop__single-item__info">
+                <!-- wrapped long description and title -->
+                <div>
+                    <strong
+                        class="shop__single-item__info--title productName"><?php echo $ticket['ticketDescription']; ?></strong>
                 </div>
-                <div class="shop__single-item__quiantity">
-                    <div class="shop__single-item__add-to-cart items priceQuantity">
-                        <span id="orderQuantityValue_<?php echo $ticket['ticketId']; ?>"
-                            class="countOrdered priceQuantity" style="font-size:14px;">0</span>
+            </div>
+            <div class="shop__single-item__image">
+                <img style="display: none;"
+                    src="https://tiqs.com/alfred/assets/images/productImages/2909_1608623878.png"
+                    alt="<?php echo $ticket['ticketDescription']; ?>">
+            </div>
+            <!-- ADDED DIV FOR + PRICE - -->
+            <div style="min-width: 270px;" class="shop__single-item__cart-wrapper">
+                <div class="shop__single-item__price items priceQuantity">
+                    <span><?php echo $ticket['ticketPrice']; ?></span>
+                </div>
+                <div class="shop__single-item__quanitity-buttons">
+                    <div class="shop__single-item__add-to-cart items priceQuantity"
+                        onclick="removeOrder('<?php echo $ticket['ticketId']; ?>', '<?php echo $ticket['ticketPrice']; ?>')">
+                        <span style="font-size:16px; vertical-align: middle; text-align:center">
+                            <i class="fa fa-minus priceQuantity" aria-hidden="true"></i>
+                        </span>
+                    </div>
+                    <div class="shop__single-item__quiantity">
+                        <div class="shop__single-item__add-to-cart items priceQuantity">
+                            <span id="orderQuantityValue_<?php echo $ticket['ticketId']; ?>"
+                                class="countOrdered priceQuantity" style="font-size:14px;">0</span>
+                        </div>
+                    </div>
+                    <div class="shop__single-item__add-to-cart items priceQuantity"
+                        onclick="addOrder('<?php echo $ticket['ticketId']; ?>', '<?php echo $ticket['ticketQuantity']; ?>', '<?php echo $ticket['ticketPrice']; ?>')">
+                        <span style="font-size:16px; vertical-align: middle; text-align:center">
+                            <i class="fa fa-plus priceQuantity" aria-hidden="true"></i>
+                        </span>
                     </div>
                 </div>
-                <div class="shop__single-item__add-to-cart items priceQuantity"
-                    onclick="addOrder('<?php echo $ticket['ticketId']; ?>', '<?php echo $ticket['ticketQuantity']; ?>', '<?php echo $ticket['ticketPrice']; ?>')">
-                    <span style="font-size:16px; vertical-align: middle; text-align:center">
-                        <i class="fa fa-plus priceQuantity" aria-hidden="true"></i>
-                    </span>
-                </div>
+                <!-- end quantity buttons -->
             </div>
-            <!-- end quantity buttons -->
         </div>
-    </div>
-    <?php endforeach; ?>
-    <div class="bottom-bar footer">
-        <div class="container">
-            <div class="row">
-                <!--				<div class="col-12 col-md-6 text-center text-left-md">-->
-                <div class="col-12 text-center text-left-md">
-                    <div style="background: red !important;" class="totalButton">
-                        <p class="button-main button-secondary bottom-bar__checkout totalButton">TOTAL: <span
-                                class="bottom-bar__total-price">€&nbsp;<span class="totalPrice">00.00</span></span> </p>
-                        <!-- <button class='button-main button-secondary' onclick="focusCheckOutModal('modal__checkout__list')">Order List</button> -->
+        <?php endforeach; ?>
+        <div class="bottom-bar footer">
+            <div class="container">
+                <div class="row">
+                    <!--				<div class="col-12 col-md-6 text-center text-left-md">-->
+                    <div class="col-12 text-center text-left-md">
+                        <div style="background: red !important;" class="totalButton">
+                            <p class="button-main button-secondary bottom-bar__checkout totalButton">TOTAL: <span
+                                    class="bottom-bar__total-price">€&nbsp;<span class="totalPrice">00.00</span></span>
+                            </p>
+                            <!-- <button class='button-main button-secondary' onclick="focusCheckOutModal('modal__checkout__list')">Order List</button> -->
+                        </div>
+                    </div>
+                    <!--				<div class="col-12 col-md-6 text-center text-right-md">-->
+                    <div style="background: red !important;" class="col-12 text-center text-right-md">
+                        <button id="pay" type="submit"
+                            class="button-main button-secondary bottom-bar__checkout payButton"
+                            style="width:100%">PAY</button>
                     </div>
                 </div>
-                <!--				<div class="col-12 col-md-6 text-center text-right-md">-->
-                <div style="background: red !important;" class="col-12 text-center text-right-md">
-                    <button type="submit" class="button-main button-secondary bottom-bar__checkout payButton"
-                        style="width:100%">PAY</button>
-                </div>
             </div>
         </div>
+
+
     </div>
-
-
-</div>
-<?php endif; ?>
+    <?php endif; ?>
 </form>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
-<script>
-
-function removeOrder(id, price) {
-    var quantityValue = $("#orderQuantityValue_" + id).text();
-    var totalPrice = $(".totalPrice").text()
-    quantityValue = parseInt(quantityValue);
-    totalPrice = parseInt(totalPrice);
-    price = parseInt(price);
-    if (quantityValue == 0) {
-        return;
-    }
-    quantityValue--;
-    totalPrice = totalPrice - price;
-    $("#orderQuantityValue_" + id).text(quantityValue);
-    $("#quantity_"+id).val(quantityValue);
-    return $(".totalPrice").text(totalPrice.toFixed(2));
-}
-
-function addOrder(id, limit, price) {
-    var quantityValue = $("#orderQuantityValue_" + id).text();
-    var totalPrice = $(".totalPrice").text()
-    quantityValue = parseInt(quantityValue);
-    totalPrice = parseInt(totalPrice);
-    price = parseInt(price);
-    limit = parseInt(limit);
-    if (quantityValue == limit) {
-        return;
-    }
-    quantityValue++;
-    totalPrice = totalPrice + price;
-    $("#orderQuantityValue_" + id).text(quantityValue);
-    $("#quantity_"+id).val(quantityValue);
-    return $(".totalPrice").text(totalPrice.toFixed(2));
-}
-$(document).ready(function(){
-    setInterval(() => {
-    var CurrentDate = moment().format();
-    $("#current_time").val(CurrentDate);
-   
-}, 1000);
-});
-
-</script>
--- application/views/events/your_tickets.php
+++ application/views/events/your_tickets.php
@@ -6,68 +6,73 @@
     </div>
 
 </div>
+
+
+
+
 <?php if($this->session->tempdata('tickets')):
         $tickets = $this->session->tempdata('tickets'); ?>
-<div style="visibility: hidden;" class="form">
-    <div class="w-100 p-2 text-right">
-        <p class="h5 font-weight-bold" id="timer"></p>
-        <br>
-    </div>
-    <?php if($this->session->has_userdata('event_date')): ?>
-    <h4 class="font-weight-bold"><?php echo $this->session->userdata('event_date'); ?></h4>
-    <?php endif; ?>
-    <div style="border: 1px solid #c7c3b9" class="w-100">
-        <?php foreach($tickets as $key => $ticket): ?>
-        <?php if($key != 0): ?>
-        <hr style="border: 1px solid #c7c3b9" class="w-100">
-        <?php endif; ?>
-        <label>Description: <?php echo $ticket['descript']; ?></label>
-        <label>Quantity: <?php echo $ticket['quantity']; ?></label>
-        <label>Amount: €<?php echo number_format($ticket['amount'], 2, '.', ''); ?></label>
-        <?php endforeach; ?>
-    </div>
-    <div class="w-100 p-2 text-right">
-        <br>
-        <p class="h5 font-weight-bold">
-            <strong>Total: €<?php echo $this->session->tempdata('total'); ?></strong>
-        </p>
+<div style="visibility: hidden;" class="limiter">
+    <div class="container-login100">
+        <div style="text-align: center;" class="wrap-login100">
+            <div class="login100-form-title">
+                <span class="login100-form-title-1">
+                    <?php if($this->session->has_userdata('event_date')): ?>
+                    <h4 class="font-weight-bold"><?php echo $this->session->userdata('event_date'); ?></h4>
+                    <?php endif; ?>
+            </div>
+            <?php foreach($tickets as $key => $ticket): ?>
+            <?php if($key != 0): ?>
+            <hr style="border: 1px solid #c7c3b9" class="w-100">
+            <?php endif; ?>
+
+            <p class="mb-3" style="text-align: right;padding: 10px;">
+                <strong id="timer"></strong>
+            </p>
+            <p>
+                <strong>Description: <?php echo $ticket['descript']; ?></strong>
+            </p>
+            <p>
+                <strong>Quantity: <?php echo $ticket['quantity']; ?></strong>
+            </p>
+            <p>
+                <strong>Amount: €<?php echo number_format($ticket['amount'], 2, '.', ''); ?></strong>
+            </p>
+
+            <?php endforeach; ?>
+
+            <p style="text-align: right;padding: 10px;">
+                <strong>Total: €<?php echo $this->session->tempdata('total'); ?></strong>
+            </p>
+
+
+            <div class="w-100 bottom-bar">
+                <a style="height: 45px;font-size: 18px;" href="<?php echo base_url(); ?>events/pay" id="next"
+                    class="btn btn-warning btn-block pt-5" type="submit">
+                    <strong>NEXT</strong>
+
+                </a>
+            </div>
+
+            <!--
+                <div class="flex-sb-m w-full p-b-30">
+                    <div class="contact100-form-checkbox">
+                        <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+                        <label class="label-checkbox100" for="ckb1">
+                            
+                        </label>
+                    </div>
+                    <div>
+                        <a href="#" class="txt1">
+                            
+                        </a>
+                    </div>
+                </div>
+                -->
+
+
+        </div>
     </div>
 </div>
-<div class="w-100 bottom-bar">
-    <a href="<?php echo base_url(); ?>events/pay" id="next" class="btn btn-success btn-block pt-5"
-        type="submit">NEXT</a>
-</div>
+
 <?php endif; ?>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
-<script>
-var countDownDate = moment("<?php echo $this->session->tempdata('exp_time'); ?>");
-
-
-var x = setInterval(function() {
-
-    var now = moment();
-
-    var distance = countDownDate - now;
-
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    document.getElementById("timer").innerHTML =
-        "Expiration time: " + addZero(minutes) + ":" + addZero(seconds) + "";
-    $('.form').css('visibility', 'visible');
-
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("timer").innerHTML = "EXPIRED";
-    }
-}, 1000);
-
-function addZero(num) {
-    let i = parseInt(num);
-    if (i < 10) {
-        num = "0" + num;
-        return num;
-    }
-    return num;
-}
-</script>
