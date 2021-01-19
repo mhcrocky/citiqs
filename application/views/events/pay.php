4735ed3b3afe56c0baddd0eb7b4467b50fb85ba5
aktwork <larastar721@gmail.com>
2021-01-19 15:05:46 +0100

    first commit

---
+++ application/views/events/pay.php
@@ -0,0 +1,35 @@
+<div style="text-align: center;" id="header-img" class="w-100" style="text-align:center">
+
+
+    <div class="form-group has-feedback">
+        <img src="<?php echo base_url(); ?>assets/home/images/tiqslogowhite.png" alt="tiqs" width="250" height="auto" />
+    </div>
+
+</div>
+
+<div class="form">
+    <div class="100">
+        <label class="email">
+            <p class="label-txt">YOUR EMAIL</p>
+            <input type="email" id="email" name="email" class="input">
+            <div class="line-box">
+                <div class="line"></div>
+            </div>
+        </label>
+        <label class="address">
+            <p class="label-txt">YOUR ADDRESS</p>
+            <input type="text" id="address" name="address" class="input">
+            <div class="line-box">
+                <div class="line"></div>
+            </div>
+        </label>
+        <label>
+            <p class="label-txt">YOUR PHONE NUMBER (OPTIONAL)</p>
+            <input type="text" class="input">
+            <div class="line-box">
+                <div class="line"></div>
+            </div>
+        </label>
+        <button id="pay" class="btn-warning btn-block" type="submit">PAY</button>
+    </div>
+</div>
