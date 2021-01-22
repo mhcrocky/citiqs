9b0ac2aa92fae1c9a6b880d527d3e5e1f4409afc
aktwork <larastar721@gmail.com>
2021-01-22 01:26:09 +0100

    first commit

---
+++ application/views/includes/footerShop.php
@@ -0,0 +1,52 @@
+
+				<!-- SECTION ORDER -->
+				<section style="display: none" class="order-section">
+				    <div class="container">
+				        <div class="row">
+				            <div class="col-12 col-md-3 px-0 d-flex flex-column banner__cta">
+				                <h6 class='py-3 px-4 px-md-4 font-weight-bold text-uppercase color-secondary'>Order by Phone</h6>
+				                <div
+				                    class='px-4 py-5 rounded-right d-flex align-items-left justify-content-center flex-grow-1 flex-column'>
+				                    <a href="tel:" class='text-white font-weight-bold py-3 d-block'>999-999-999</a>
+				                </div>
+				            </div>
+				            <!-- end col -->
+				            <div class="col-12 col-md-9 px-0 pr-0 d-flex flex-column text-left mt-4 mt-md-0">
+				                <h6 class='py-3 px-4 px-md-5 font-weight-bold text-muted text-uppercase'>Don't miss out</h6>
+				                <div
+				                    class='order-section__latest py-5 px-4 px-md-5 d-flex align-items-left justify-content-center flex-grow-1 flex-column'>
+				                    <h4 class='font-weight-bold'>Lorem Ipsum</h4>
+				                    <p class='latest__exceprtion'><span class='latest__meta'>December 12, 2020</span>Excepteur sint
+				                        occaecat cupidatat non proident, sunt in culpa ut aliquid ex ea commodi consequatur? Quis
+				                        autem vel eum iure reprehenderit qui in ea voluptate.</p>
+				                </div>
+				            </div>
+				            <!-- end col -->
+				        </div>
+				    </div>
+				</section>
+				<!-- END SECTION ORDER -->
+
+				<footer class='footer bg-light-gray'>
+				    <div class="container text-center">
+				        <div class="row footer__row">
+				            <div class="col-12 col-md-6 text-center">
+				                TIQS <?php echo date('Y'); ?>, All rights reserved
+				            </div>
+				        </div>
+				        <!-- end row -->
+				        <!--<div class="row footer__row">
+    			<div class="col-12 col-md-6">
+    				<p class='mb-0'>Lorem Ipsum</p>
+    			</div>
+    		</div>-->
+				    </div>
+				</footer>
+
+
+				<!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+				<?php include_once FCPATH . 'application/views/includes/customJs.php'; ?>
+
+				</body>
+
+				</html>
