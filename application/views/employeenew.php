a6f7ccc3d4160e6025d7d429af58b98e57df9b9f
aktwork <larastar721@gmail.com>
2021-02-06 23:54:02 +0100

    modified menu options modal's width

--- application/views/employeenew.php
+++ application/views/employeenew.php
@@ -269,7 +269,7 @@
                 <!-- MENU OPTIONS Modal -->
                 <div class="modal fade" id="editMenuOptionsModal<?php echo $employee->id; ?>Modal" tabindex="-1"
                     role="dialog" aria-labelledby="editMenuOptionsLabel" aria-hidden="true">
-                    <div class="modal-dialog modal-md" role="document">
+                    <div class="modal-dialog modal-lg" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
                                 <h5 class="modal-title">Choose Menu Options</h5>
