2c23c3143c301d8d9fa122e6a36b99ab2e3938ad
aktwork <larastar721@gmail.com>
2021-02-11 23:47:37 +0100

    made some changes

--- application/views/includes/newbookingfooter.php
+++ application/views/includes/newbookingfooter.php
@@ -1,141 +1,186 @@
 <!-- end booking form inputs -->
 <div id="booking-footer" class="booking-form__result w-100">
-			<h4 id="footer-title" class="mb-3">Booking Info </h4>
-			<p class="booking-info">Event Date: <span id="selected-date">
-			<?php $shorturl = $this->session->userdata('shortUrl');
+    <h4 id="footer-title" class="mb-3">Booking Info </h4>
+    <p class="booking-info">Event Date: <span id="selected-date">
+            <?php $shorturl = $this->session->userdata('shortUrl');
 			 if(base_url("agenda_booking/$shorturl") != current_url()): ?>
-			    <?php echo $this->session->userdata('date'); ?>
-			<?php endif; ?>
-			</span></p>
-			<p class="booking-info">SPOT Description: <span id="spot"><?php echo $this->session->userdata('spotDescript'); ?></span></p>
-			<p class="booking-info">SPOT Price: <span id="price"><?php echo $this->session->userdata('spotPrice'); ?></span></p>
-			<p class="booking-info">Time Slot: <span id="selected-time"><?php echo $this->session->userdata('timeslot'); ?></span></p>
-			<p class="booking-info">Personal Info: <span id="personal-info"></span></p>
-		</div>
-		<!-- end booking form results -->
-	</div>
-	<!-- end booking form -->
+            <?php echo $this->session->userdata('date'); ?>
+            <?php endif; ?>
+        </span></p>
+    <p class="booking-info">SPOT Description: <span
+            id="spot"><?php echo $this->session->userdata('spotDescript'); ?></span></p>
+    <p class="booking-info">SPOT Price: <span id="price"><?php echo $this->session->userdata('spotPrice'); ?></span></p>
+    <p class="booking-info">Time Slot: <span
+            id="selected-time"><?php echo $this->session->userdata('timeslot'); ?></span></p>
+    <p class="booking-info">Personal Info: <span id="personal-info"></span></p>
+</div>
+<!-- end booking form results -->
+</div>
+<!-- end booking form -->
 
 
 
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+</script>
 <script src="<?php echo base_url(); ?>assets/js/toastr.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/datepicker/js/bootstrap-datepicker.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/dayjs/1.10.4/dayjs.min.js"
+    integrity="sha512-0fcCRl828lBlrSCa8QJY51mtNqTcHxabaXVLPgw/jPA5Nutujh6CbTdDgRzl9aSPYW/uuE7c4SffFUQFBAy6lg=="
+    crossorigin="anonymous"></script>
 
 <!-- datepicker -->
 <script>
-$(document).ready(function(){
-	
-	var url = '<?php echo current_url(); ?>';
-	
-	if (url.includes('spots')) {
-		$("#spot-active").addClass('booking-active');
-		console.log('spots');
-	} else if(url.includes('timeslot')) {
-		$("#timeslot-active").addClass('booking-active');
-	} else if(url.includes('time')) {
-		$("#timeslot-active").addClass('booking-active');
-	} else if(url.includes('pay')) {
-		$("#personal-active").addClass('booking-active');
-	} else {
-		$("#agenda-active").addClass('booking-active');
-	}
+$(document).ready(function() {
+    var agenda_dates = JSON.parse('<?php echo json_encode($agenda_dates); ?>');
+
+
+    $('.day').each(function() {
+        var date = $(this).attr('data-daydate');
+        if (agenda_dates.includes(date)) {
+			console.log(date);
+            $(this).addClass('day-agenda');
+        }
+    });
+
+
+    var url = '<?php echo current_url(); ?>';
+
+    if (url.includes('spots')) {
+        $("#spot-active").addClass('booking-active');
+        console.log('spots');
+    } else if (url.includes('timeslot')) {
+        $("#timeslot-active").addClass('booking-active');
+    } else if (url.includes('time')) {
+        $("#timeslot-active").addClass('booking-active');
+    } else if (url.includes('pay')) {
+        $("#personal-active").addClass('booking-active');
+    } else {
+        $("#agenda-active").addClass('booking-active');
+    }
 
 });
-	
-	/* disable previous dates */
-	var todayDate = new Date();
-	todayDate.setDate(todayDate.getDate()-1);
-	
-	$('#date-input').datepicker({
-		startDate: todayDate
-	});
-    
-   
-	/* get date from calendar on click */
-	$('#date-input').on('changeDate', function() {
-        let getDate = $('#date-input').datepicker('getFormattedDate');
-        let d = getDate.split('/');
-        let mm = d[0];
-        let dd = d[1];
-        let yy = d[2];
-        let date = yy + '-' + mm + '-' + dd;
-		$.post('<?php echo base_url(); ?>agenda_booking/get_agenda/<?php echo isset($shortUrl) ? $shortUrl : ''; ?>',{date:date},function(data){
-            data = JSON.parse(data);
-            if(typeof data[0] === 'undefined'){
-              toastr.options = {
+
+/* disable previous dates */
+var todayDate = dayjs().format();
+
+$('#date-input').datepicker({
+    startDate: todayDate
+});
+
+
+
+$("#date-input").on('changeMonth', function() {
+    setTimeout(() => {
+		console.log($('.day'));
+        var agenda_dates = JSON.parse('<?php echo json_encode($agenda_dates); ?>');
+        $('.day').each(function() {
+            var date = $(this).attr('data-daydate');
+            if (agenda_dates.includes(date)) {
+                $(this).addClass('day-agenda');
+            }
+        });
+    }, 20);
+
+});
+
+/* get date from calendar on click */
+$('#date-input').on('changeDate', function() {
+    var agenda_dates = JSON.parse('<?php echo json_encode($agenda_dates); ?>');
+    $('.day').each(function() {
+        var date = $(this).attr('data-daydate');
+        if (agenda_dates.includes(date)) {
+            $(this).addClass('day-agenda');
+        }
+    });
+
+    let getDate = $('#date-input').datepicker('getFormattedDate');
+
+    let d = getDate.split('/');
+    let mm = d[0];
+    let dd = d[1];
+    let yy = d[2];
+    let date = yy + '-' + mm + '-' + dd;
+    $.post('<?php echo base_url(); ?>agenda_booking/get_agenda/<?php echo isset($shortUrl) ? $shortUrl : ''; ?>', {
+        date: date
+    }, function(data) {
+        data = JSON.parse(data);
+        if (typeof data[0] === 'undefined') {
+            toastr.options = {
                 "showDuration": "200",
                 "hideDuration": "1000",
                 "timeOut": "1000"
-              }
-              toastr["error"]("Sorry, we don't have agenda for this date!");
-                return ;
             }
-            let agenda_id = data[0]['id'];
-            let dateFormat = date.replace('-', '').replace('-', '');
-            location.href = "<?php echo base_url(); ?>agenda_booking/spots/"+dateFormat+"/"+agenda_id;
-        });
-	});
-	
-	/* get number of persons on click */
-	$('#person-input .form-check-label').on('click', function() {
-		$('#selected-persons').text($(this).siblings('input').val());
-		$('.step-2').removeClass('active');
-		$('.step-3').addClass('active');
-	});
-	
-	/* get time slot on click */
-	$('#time-input .form-check-label').on('click', function() {
-		$('#selected-time').text($(this).siblings('input').val());
-		$('.step').removeClass('active');
-		$('.step-4').addClass('active');
-	});
-	
-	/* show value when input field lose focus */ 
-	$('#name-input').blur(function(){
-		$('#personal-name').text($(this).val());
-	})
-	
-	$('#email-input').blur(function(){
-		$('#personal-email').text(', ' + $(this).val());
-	})
-	
-	$('#phone-input').blur(function(){
-		$('#personal-phone').text(', ' + $(this).val());
-	})
-	
-	
-	
-	/* switch steps by clicking on title */
-	$('.booking-form__date-link').click(function(){
-		$('.step').removeClass('active');
-		$('.step-1').addClass('active');
-	})
-	
-	$('.booking-form__person-link').click(function(){
-		$('.step').removeClass('active');
-		$('.step-2').addClass('active');
-	})
-	
-	$('.booking-form__time-link').click(function(){
-		$('.step').removeClass('active');
-		$('.step-3').addClass('active');
-	})
-	
-	$('.booking-form__info-link').click(function(){
-		$('.step').removeClass('active');
-		$('.step-4').addClass('active');
-	})
-	
+            toastr["error"]("Sorry, we don't have agenda for this date!");
+            return;
+        }
+        let agenda_id = data[0]['id'];
+        let dateFormat = date.replace('-', '').replace('-', '');
+        location.href = "<?php echo base_url(); ?>agenda_booking/spots/" + dateFormat + "/" + agenda_id;
+    });
+});
 
-	/* go back button */
-	$('.go-back-button').click(function(){
-		$(this).parents('.step').removeClass('active');
-		$(this).parents('.step').prev().addClass('active')
-	})
-	
+/* get number of persons on click */
+$('#person-input .form-check-label').on('click', function() {
+    $('#selected-persons').text($(this).siblings('input').val());
+    $('.step-2').removeClass('active');
+    $('.step-3').addClass('active');
+});
+
+/* get time slot on click */
+$('#time-input .form-check-label').on('click', function() {
+    $('#selected-time').text($(this).siblings('input').val());
+    $('.step').removeClass('active');
+    $('.step-4').addClass('active');
+});
+
+/* show value when input field lose focus */
+$('#name-input').blur(function() {
+    $('#personal-name').text($(this).val());
+})
+
+$('#email-input').blur(function() {
+    $('#personal-email').text(', ' + $(this).val());
+})
+
+$('#phone-input').blur(function() {
+    $('#personal-phone').text(', ' + $(this).val());
+})
+
+
+
+/* switch steps by clicking on title */
+$('.booking-form__date-link').click(function() {
+    $('.step').removeClass('active');
+    $('.step-1').addClass('active');
+})
+
+$('.booking-form__person-link').click(function() {
+    $('.step').removeClass('active');
+    $('.step-2').addClass('active');
+})
+
+$('.booking-form__time-link').click(function() {
+    $('.step').removeClass('active');
+    $('.step-3').addClass('active');
+})
+
+$('.booking-form__info-link').click(function() {
+    $('.step').removeClass('active');
+    $('.step-4').addClass('active');
+})
+
+
+/* go back button */
+$('.go-back-button').click(function() {
+    $(this).parents('.step').removeClass('active');
+    $(this).parents('.step').prev().addClass('active')
+})
 </script>
 
 </body>
+
 </html>
