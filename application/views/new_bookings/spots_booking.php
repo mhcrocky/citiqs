d003d9d500263e0d86de4adbb528c4f96f98ca17
aktwork <larastar721@gmail.com>
2021-02-15 18:50:02 +0100

    modified card's style

--- application/views/new_bookings/spots_booking.php
+++ application/views/new_bookings/spots_booking.php
@@ -1,8 +1,7 @@
-<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/css/card-style.css">
 <?php $backgroundColors = ['background-blue-light', 'background-blue', 'background-orange-light', 'background-purple-light', 'background-orange']; ?>
-    <div class="col-12 step step-2 active" id="person-input">
-        <h3 id="title">Choose an available SPOT:</h3>
-        <?php 
+<div class="col-12 step step-2 active" id="person-input">
+    <h3 id="title">Choose an available SPOT:</h3>
+    <?php 
         $last = $this->uri->total_segments();
         $agenda_id = $this->uri->segment($last);
         if($spots){
@@ -22,62 +21,67 @@
 
         }
         
-
         ?>
-            <?php $count = 0; ?>
-            <?php $backgroundCount = 0; ?>
-            <?php if($spots_exist): ?>
-                <div class="social-box">
-                    <div class="container text-center">
-                        
-                        <div class="row">
+    <?php $count = 0; ?>
+    <?php $backgroundCount = 0; ?>
+    <?php if($spots_exist): ?>
+    <div class="w-100 social-box">
+        <div class="w-100 container text-center">
             <?php foreach($spots as $key=>$spot): ?>
-
-                    
-
+            <div class="row">
 
 
-                    <div class="col-lg-12 col-xs-12 text-center">
-					<div id="card" class="box" onClick="window.location.replace('<?php echo $this->baseUrl; ?>agenda_booking/time_slots/<?php echo $spot["data"]->id; ?>')">
-                        <img style="filter: invert(70%);" width="auto" height="50px" src="<?php echo $this->baseUrl; ?>assets/home/images/<?php echo $spot['data']->image; ?>" >
-						<div class="box-title">
-							<h3 id="spot-title"><?php echo $spot['data']->descript; ?></h3>
-						</div>
+
+
+
+
+                <div class="w-100 text-center">
+                    <div id="card" class="w-100 box"
+                        onclick="window.location.replace('<?php echo $this->baseUrl; ?>agenda_booking/time_slots/<?php echo $spot["data"]->id; ?>')">
+                        <img style="filter: invert(70%);" width="auto" height="50px"
+                            src="<?php echo $this->baseUrl; ?>assets/home/images/<?php echo $spot['data']->image; ?>">
+                        <div class="box-title">
+                            <h3 id="spot-title"><?php echo $spot['data']->descript; ?></h3>
+                        </div>
                         <div class="box-text">
-							<span class="spot-data"><?php echo date("d.m.Y", strtotime($eventDate)) ?></span>
-						</div>
-						<div class="box-text">
-							<span class="spot-data"><?php echo $spot['data']->pricingdescript; ?> € <?php echo number_format($spot['data']->price, 2); ?></span>
-						</div>
-						<div class="box-btn">
-                        <a id="choose-time" href="<?php echo $this->baseUrl; ?>agenda_booking/time_slots/<?php echo $spot['data']->id; ?>" class="button button-orange mb-25"><?= ($this->language->Line("SPOTS-BOOKING-0005", "CHOOSE A TIME")) ? $this->language->Line("SPOTS-BOOKING-0005", "CHOOSE A TIME") : 'Next'; ?></a>
-                    </div>
-					 </div>
-				</div>
-
-
-
-                    <?php $backgroundCount++; ?>
-                    <?php if($backgroundCount == count($backgroundColors)): ?>
-                        <?php $backgroundCount = 0; ?>
-                    <?php endif; ?>
-            <?php endforeach; ?>
+                            <span class="spot-data"><?php echo date("d.m.Y", strtotime($eventDate)) ?></span>
+                        </div>
+                        <div class="box-text">
+                            <span class="spot-data"><?php echo $spot['data']->pricingdescript; ?> €
+                                <?php echo number_format($spot['data']->price, 2); ?></span>
+                        </div>
+                        <div class="box-btn">
+                            <a id="choose-time"
+                                href="<?php echo $this->baseUrl; ?>agenda_booking/time_slots/<?php echo $spot['data']->id; ?>"
+                                class="button button-orange mb-25"><?= ($this->language->Line("SPOTS-BOOKING-0005", "CHOOSE A TIME")) ? $this->language->Line("SPOTS-BOOKING-0005", "CHOOSE A TIME") : 'Next'; ?></a>
+                        </div>
                     </div>
                 </div>
+
+
+
+                <?php $backgroundCount++; ?>
+                <?php if($backgroundCount == count($backgroundColors)): ?>
+                <?php $backgroundCount = 0; ?>
+                <?php endif; ?>
+
             </div>
-            <?php else: ?>
-                <div class="social-box">
-                    <div class="container text-center">
-                        <h3 class="text-content mb-50">
-                            Sorry, we don't have any SPOT available for this date!
-                        </h3>
-                    </div>
-                </div>
-            <?php endif; ?>
-		<div class="w-100 go-back-wrapper">
-			<a class="go-back-button" href="javascript:history.back()">Go Back</a>
-		</div>
-	</div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+    <?php else: ?>
+    <div class="social-box">
+        <div class="container text-center">
+            <h3 class="text-content mb-50">
+                Sorry, we don't have any SPOT available for this date!
+            </h3>
+        </div>
+    </div>
+    <?php endif; ?>
+    <div class="w-100 go-back-wrapper">
+        <a class="go-back-button" href="javascript:history.back()">Go Back</a>
+    </div>
+</div>
 <script>
-    
+
 </script>
