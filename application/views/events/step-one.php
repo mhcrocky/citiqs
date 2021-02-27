8531f60f44f6623e4da0fa029472c89e5c85bb3b
aktwork <larastar721@gmail.com>
2021-02-27 03:10:12 +0100

    made a small change

--- application/views/events/step-one.php
+++ application/views/events/step-one.php
@@ -137,8 +137,7 @@
                             <div class="form-group row wideField">
                                 <label for="venue" class="col-md-4 col-form-label text-md-left">Venue</label>
                                 <div id="locationField" class="col-md-6">
-                                    <input type="text" id="autocomplete" onFocus="geolocate()"
-                                        class="form-control input-w border-50" name="eventVenue"
+                                    <input type="text" class="form-control input-w border-50" name="eventVenue"
                                         placeholder="Enter a location" required>
                                 </div>
                             </div>
@@ -146,8 +145,9 @@
                             <div class="form-group row wideField">
                                 <label for="address" class="col-md-4 col-form-label text-md-left">Address</label>
                                 <div class="col-md-6">
-                                    <input type="text" id="route" class="field form-control input-w border-50"
-                                        name="eventAddress" required>
+                                    <input type="text" id="autocomplete" onFocus="geolocate()"
+                                        class="field form-control input-w border-50" name="eventAddress" placeholder=""
+                                        required>
                                 </div>
                             </div>
 
@@ -249,85 +249,4 @@
         </div>
     </div>
     </div>
-<input type="hidden" id="street_number">
-<input type="hidden" id="administrative_area_level_1">
 
-    <script
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCst0EJ-LFVj3q0a6NHGFDU6HQ10H84HTI&callback=initAutocomplete&libraries=places&v=weekly"
-        async></script>
-
-    
-<script>
-    // This sample uses the Autocomplete widget to help the user select a
-    // place, then it retrieves the address components associated with that
-    // place, and then it populates the form fields with those details.
-    // This sample requires the Places library. Include the libraries=places
-    // parameter when you first load the API. For example:
-    // <script
-    // src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-    let placeSearch;
-    let autocomplete;
-    const componentForm = {
-        street_number: "short_name",
-        route: "long_name",
-        locality: "long_name",
-        administrative_area_level_1: "short_name",
-        country: "long_name",
-        postal_code: "short_name",
-    };
-
-    function initAutocomplete() {
-        // Create the autocomplete object, restricting the search predictions to
-        // geographical location types.
-        autocomplete = new google.maps.places.Autocomplete(
-            document.getElementById("autocomplete"), {
-                types: ["geocode"]
-            }
-        );
-        // Avoid paying for data that you don't need by restricting the set of
-        // place fields that are returned to just the address components.
-        autocomplete.setFields(["address_component"]);
-        // When the user selects an address from the drop-down, populate the
-        // address fields in the form.
-        autocomplete.addListener("place_changed", fillInAddress);
-    }
-
-    function fillInAddress() {
-        // Get the place details from the autocomplete object.
-        const place = autocomplete.getPlace();
-
-        for (const component in componentForm) {
-            document.getElementById(component).value = "";
-            document.getElementById(component).disabled = false;
-        }
-
-        // Get each component of the address from the place details,
-        // and then fill-in the corresponding field on the form.
-        for (const component of place.address_components) {
-            const addressType = component.types[0];
-
-            if (componentForm[addressType]) {
-                const val = component[componentForm[addressType]];
-                document.getElementById(addressType).value = val;
-            }
-        }
-    }
-
-    // Bias the autocomplete object to the user's geographical location,
-    // as supplied by the browser's 'navigator.geolocation' object.
-    function geolocate() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((position) => {
-                const geolocation = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude,
-                };
-                const circle = new google.maps.Circle({
-                    center: geolocation,
-                    radius: position.coords.accuracy,
-                });
-                autocomplete.setBounds(circle.getBounds());
-            });
-        }
-    }
-</script>
