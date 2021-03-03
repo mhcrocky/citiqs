f94ca1f413d85ff71907edd0badb9acf3c999cfb
aktwork <larastar721@gmail.com>
2021-03-03 20:54:22 +0100

    fixed a issue with height

--- assets/home/js/selectSpot.js
+++ assets/home/js/selectSpot.js
@@ -1,4 +1,10 @@
 'use strict';
+(function(){
+    $('html').height('100%');
+    $('body').height('100%');
+    $('.main-wrapper-nh').height('100%');
+}());
+
 function redirectTo(urlPath) {
     let url = globalVariables.baseUrl + urlPath;
     window.location.replace(url);
