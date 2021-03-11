da320e0096e26e151b62e10fd09d7d1fc8498da9
aktwork <larastar721@gmail.com>
2021-03-11 22:32:03 +0100

    added custom voucher code field

--- application/controllers/Voucher.php
+++ application/controllers/Voucher.php
@@ -14,7 +14,28 @@ class Voucher extends BaseControllerWeb
 
 	public function index(){
 		$this->global['pageTitle'] = 'TIQS: Create Vouchers';
-        $data['vendorId'] = $this->session->userdata('userId');
+		$vendorId = $this->session->userdata('userId');
+        $data['vendorId'] = $vendorId;
+		$this->load->model('shopproduct_model');
+		$join = [
+			0 => [
+				'tbl_shop_products_extended',
+				'tbl_shop_products_extended.productId = tbl_shop_products.id',
+				'left',
+			],
+			1 => [
+				'tbl_shop_categories',
+				'tbl_shop_categories.id = tbl_shop_products.categoryId',
+				'left',
+			]
+		];
+		$what = ['tbl_shop_products.id' ,'tbl_shop_products_extended.name'];
+		$where = [
+			 "userId" => $vendorId,
+			 "tbl_shop_products_extended.name<>" => null
+			];
+			
+		$data['products'] = $this->shopproduct_model->read($what,$where, $join, 'group_by', ['tbl_shop_products.id']);
 		$this->loadViews("voucher/create", $this->global, $data, 'footerbusiness', 'headerbusiness'); 
 	}
 
