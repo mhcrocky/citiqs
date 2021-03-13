f9cb986c61cd5d72e2d6b8f19dfc33a4d29f6b0d
aktwork <larastar721@gmail.com>
2021-03-13 21:53:12 +0100

    first commit

---
+++ assets/home/js/vouchersDataTable.js
@@ -0,0 +1,75 @@
+$(document).ready(function () {
+
+  var table = $("#report").DataTable({
+    processing: true,
+    lengthMenu: [
+      [5, 10, 20, 50, 100, 200, 500, -1],
+      [5, 10, 20, 50, 100, 200, 500, "All"],
+    ],
+    pageLength: 5,
+    ajax: {
+      type: "get",
+      url: globalVariables.baseUrl + "api/voucher/vouchers",
+      dataSrc: "",
+    },
+    columns: [
+      {
+        title: "ID",
+        data: "id",
+      },
+      {
+        title: "Code",
+        data: "code",
+      },
+      {
+        title: "Description",
+        data: "description",
+      },
+      {
+        title: "Percent",
+        data: "percent",
+      },
+      {
+        title: "Amount",
+        data: "amount",
+      },
+      {
+        title: "Percent Used",
+        data: "percentUsed",
+      },
+      {
+        title: "Active",
+        data: null,
+        "render": function (data, type, row) {
+          if(data.active == 1){
+            return 'Yes';
+          }
+          return 'No';
+        }
+      },
+      {
+        title: "Number Of Times",
+        data: "numberOfTimes",
+      },
+      {
+        title: "Activated",
+        data: null,
+        "render": function (data, type, row) {
+          if(data.activated == 1){
+            return 'Activated';
+          }
+          return 'Deactivate';
+        }
+      },
+      {
+        title: "Product Id",
+        data: "productId",
+      }
+    ],
+  });
+
+  $('#report_filter').addClass('text-right');
+  $('#report_filter label').addClass('text-left');
+
+});
+  
