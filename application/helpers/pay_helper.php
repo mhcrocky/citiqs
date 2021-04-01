3c765ae73a6155d848715cae214cc30a5e69ca49
aktwork <larastar721@gmail.com>
2021-04-01 23:16:52 +0200

    modified exchangepay function

--- application/helpers/pay_helper.php
+++ application/helpers/pay_helper.php
@@ -215,15 +215,6 @@
                 }
 
 
-                $arrArguments['statsData']['extra' . ($key + 1)] = $reservation->reservationId;
-                $arrArguments['saleData']['orderData'][$key]['productId'] = $reservation->reservationId;
-                $arrArguments['saleData']['orderData'][$key]['description'] = $reservation->Spotlabel;
-                $arrArguments['saleData']['orderData'][$key]['productType'] = 'HANDLIUNG';
-                $arrArguments['saleData']['orderData'][$key]['price'] = $reservation->price * 100;
-                $arrArguments['saleData']['orderData'][$key]['quantity'] = 1;
-                $arrArguments['saleData']['orderData'][$key]['vatCode'] = 'H';
-                $arrArguments['saleData']['orderData'][$key]['vatPercentage'] = '0.00';
-
             }
 
             $payment = self::getPaymentMethod($paymentType);
@@ -255,7 +246,7 @@
 
             $arrArguments['finishUrl'] = base_url() . 'bookingpay/successBooking/';
 
-            $arrArguments['transaction']['orderExchangeUrl'] = base_url() . 'bookingpay/exchangePay';
+            $arrArguments['transaction']['orderExchangeUrl'] = base_url() . 'bookingpay/exchangePay/';
 
 
             $arrArguments['statsData']['promotorId'] = $vendorId;
