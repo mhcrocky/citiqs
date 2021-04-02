abca5e61f40aaeff6a50c3836d5e7771bac6e23c
aktwork <larastar721@gmail.com>
2021-04-02 21:38:51 +0200

    free amoutn amount refund replace places

--- application/views/businessreport/reports.php
+++ application/views/businessreport/reports.php
@@ -81,6 +81,12 @@
                 <div style="padding: 0px; padding-top: 1rem;" class="modal-body">
                     <div class="w-100 table-responsive" id="productsRefund"></div>
                     <div style="flex-wrap: unset;" class="row pl-4 pr-1 pt-4">
+                        <div class="col-md-4 pt-2 font-weight-bold">Amount (€<span id="order_amount"></span>):</div>
+                        <div class="col-md-8">
+                            <input type="text" style="max-width: 75px;padding-left: 5px;padding-right: 5px;" class="form-control ml-auto" id="amount" value="0" disabled>
+                        </div>
+                    </div>
+                    <div style="flex-wrap: unset;" class="row pl-4 pr-1 pt-4">
                         <div class="col-md-4 pt-2 font-weight-bold">Free Amount:</div>
                         <div style="flex-wrap: unset" class="col-md-8 input-group">
                             <input type="hidden" id="amount_limit">
@@ -88,13 +94,6 @@
                             <input type="number" max="0" onchange="freeAmountValidate(this)" style="max-width: 53px;width: 53px;padding-left: 0px;padding-right: 5px;border-left: 0px;" class="form-control" id="freeamount" name="freeamount" value="0.00">
                         </div>
                     </div>
-                    <div style="flex-wrap: unset;" class="row pl-4 pr-1 pt-4">
-                        <div class="col-md-4 pt-2 font-weight-bold">Amount (€<span id="order_amount"></span>):</div>
-                        <div class="col-md-8">
-                            <input type="text" style="max-width: 75px;padding-left: 5px;padding-right: 5px;" class="form-control ml-auto" id="amount" value="0" disabled>
-                        </div>
-                    </div>
-
                     <div style="flex-wrap: unset;" class="row pl-4 pr-1 pt-4 pb-4">
                         <div class="col-md-4 pt-2 font-weight-bold">Description:</div>
                         <div class="col-md-8 input-group"><input type="text" class="form-control" id="description" disabled></div>
