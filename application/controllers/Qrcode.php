94ac2fa033378415ef534b0978668102097a2eeb
aktwork <larastar721@gmail.com>
2021-04-07 20:35:32 +0200

    first commit

---
+++ application/controllers/Qrcode.php
@@ -0,0 +1,48 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . '/libraries/BaseControllerWeb.php';
+
+class Qrcode extends BaseControllerWeb {
+
+    
+    public function __construct(){
+        parent::__construct();
+        $this->load->model('qrcode_model');
+        $this->load->library('language', array('controller' => $this->router->class));
+        $this->load->library('session');
+        $this->isLoggedIn();
+    }
+
+    public function index() {
+        $this->global['pageTitle'] = 'TIQS QRCode';
+        $this->loadViews("qrcodeview/index", $this->global, '', 'footerbusiness', 'headerbusiness');
+    }
+
+    public function get_qrcodes() {
+        $vendorId = $this->session->userdata('userId');
+        $qrcodes = $this->qrcode_model->get_qrcodes($vendorId);
+        echo json_encode($qrcodes);
+
+    }
+
+    public function save_qrcode() {
+        $data = $this->input->post(null, true);
+        $vendorId = $this->session->userdata('userId');
+        $data['vendorId'] = $vendorId;
+        return $this->qrcode_model->save_qrcode($data);
+
+    }
+
+    public function update_qrcode() {
+        $qrcodeId = $this->input->post('qrcodeId');
+        $spot = $this->input->post('spot');
+        $vendorId = $this->session->userdata('userId');
+        return $this->qrcode_model->update_qrcode($spot, $qrcodeId, $vendorId);
+
+    }
+    
+
+
+}
