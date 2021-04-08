dd0117649b6fbf1af762ef2a22fbbe61672887a5
aktwork <larastar721@gmail.com>
2021-04-08 22:57:01 +0200

    fixed the issue with email update

--- assets/home/js/templates.js
+++ assets/home/js/templates.js
@@ -529,9 +529,9 @@ function showTemplates() {
 
 function customUpdateEmailTemplate(selectTemplateValueId, customTemplateNameId, customTemplateSubjectId, customTemplateTypeId, templateId = 0){
     createEmailTemplate(selectTemplateValueId, customTemplateNameId, customTemplateSubjectId, customTemplateTypeId, templateId);
-    // setTimeout(() => {
-    //     window.location.reload();
-    // }, 1200);
+    setTimeout(() => {
+      window.location.reload();
+    }, 1500);
 }
 
 function checkiIsLandingPage(element, landingPage, emailElements, landingPageElemenst) {
