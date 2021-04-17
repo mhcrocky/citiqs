48821f6bc9daa402305fbc96f96e0a992074bdf5
aktwork <larastar721@gmail.com>
2021-04-17 19:14:46 +0200

    first commit

---
+++ application/helpers/pdf_helper.php
@@ -0,0 +1,52 @@
+<?php
+    declare(strict_types=1);
+
+    require APPPATH . '/libraries/Pdf.php';
+
+    if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+    Class Pdf_helper
+    {
+
+        public static function HtmlToPdf($html)
+        {
+            $pdf = new Pdf(PDF_PAGE_ORIENTATION, PDF_UNIT, 'a4', true, 'UTF-8', true);
+
+
+            $pdf->SetCreator(PDF_CREATOR);
+            $pdf->SetAuthor('Tiqs');
+            $pdf->SetTitle('Reservation');
+            
+            // set header and footer fonts
+            $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+            $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+            
+            // set default monospaced font
+            $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+            
+            // set margins
+            $pdf->SetMargins(PDF_MARGIN_LEFT, 10, PDF_MARGIN_RIGHT);
+            $pdf->SetHeaderMargin(10);
+            $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+            
+            // set auto page breaks
+            
+            
+            // set image scale factor
+            $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+            
+            // add a page
+            $pdf->AddPage();
+            
+             
+            $pdf->writeHTML($html, true, false, true, false, '');
+            
+            // reset pointer to the last page
+            $pdf->lastPage();
+            //Close and output PDF document
+            
+            $pdf->Output(APPPATH . 'reservation.pdf', 'FD');
+            unlink(APPPATH . 'reservation.pdf');
+        }
+        
+    }
---
+++ application/libraries/Pdf.php
@@ -0,0 +1,27 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+require_once dirname(__FILE__) . '/tcpdf/tcpdf.php';
+
+class Pdf extends TCPDF
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function Header() {
+        $this->SetTextColor(209,183,49);
+        $this->Ln(5);        
+        $this->Cell(278, 15, '', 0, false, 'C', 0, '', 0, false, 'M', 'M');
+    }
+  
+    public function Footer() {
+        $this->SetY(-15);
+        $this->SetFont('helvetica', 'I', 10);
+        $this->Cell(278, 15, '', 0, false, 'C', 0, '', 0, false, 'M', 'M');
+  
+    }
+}
+
+/* End of file Pdf.php */
+/* Location: ./application/libraries/Pdf.php */
