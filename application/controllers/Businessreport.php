f1c48f3aa46a124370a5ad0da8a4b935d30d813c
aktwork <larastar721@gmail.com>
2021-04-20 00:27:36 +0200

    made small change

--- application/controllers/Businessreport.php
+++ application/controllers/Businessreport.php
@@ -59,7 +59,7 @@ class Businessreport extends BaseControllerWeb
 
 	public function get_report(){
 		ini_set('memory_limit','1024M');
-		$vendor_id = $this->vendor_id;//418
+		$vendor_id = $this->vendor_id;
 		$sql = $this->input->post('sql');
 		$pickup = $this->businessreport_model->get_pickup_report($vendor_id, $sql);
 		$delivery = $this->businessreport_model->get_delivery_report($vendor_id, $sql);
