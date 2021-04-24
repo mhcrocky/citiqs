2b310ce72386381dcaccd90e18fc4c808a783e7e
aktwork <larastar721@gmail.com>
2021-04-24 03:39:46 +0200

    made a small change

--- application/controllers/Booking_events.php
+++ application/controllers/Booking_events.php
@@ -81,9 +81,6 @@ class Booking_events extends BaseControllerWeb
         } 
         $vendor_id = $this->session->userdata('customer');
         $this->session->unset_userdata("event_date");
-        $this->global['pageTitle'] = 'TIQS: Step Two';
-        $design = $this->event_model->get_design($vendor_id);
-        $this->global['design'] = unserialize($design[0]['shopDesign']);
         $event = $this->event_model->get_event($vendor_id,$eventId);
         $event_start =  date_create($event->StartDate . " " . $event->StartTime);
         $event_end = date_create($event->EndDate . " " . $event->EndTime);
@@ -109,59 +106,6 @@ class Booking_events extends BaseControllerWeb
 				->set_output(json_encode($result));
 			}
         $this->loadViews("events/tickets", $this->global, $data, 'footerShop', 'headerShop');
-
-    }
-
-    public function your_tickets()
-    {
-        $this->global['pageTitle'] = 'TIQS: Your Tickets';
-        $vendor_id = $this->session->userdata('customer');
-        $results = $this->input->post(null, true);
-        if(count($results) > 0){
-            $total = 0;
-            $quantities = $results['quantity'];
-            $id = $results['id'];
-            $descript = $results['descript'];
-            $price = $results['price'];
-            $tickets = [];
- 
-            foreach($quantities as $key => $quantity){
-                if($quantity == '0'){ continue; }
-                $amount = floatval($price[$key])*floatval($quantity);
-                $total = $total + $amount;
-                    $tickets[$id[$key]] = [
-                        'id' => $id[$key],
-                        'descript' => $descript[$key],
-                        'quantity' => $quantity,
-                        'price' => $price[$key],
-                        'amount' => $amount,
-                        'startDate' => $this->session->userdata("startDate"),
-                        'startTime' => $this->session->userdata("startTime"),
-                        'endDate' => $this->session->userdata("endDate"),
-                        'endTime' => $this->session->userdata("endTime")
-                    ];
-            }
-            if($this->session->tempdata('tickets')){
-                $old_tickets = $this->session->tempdata('tickets');
-                $tickets1 = $this->check_diff_multi($tickets, $old_tickets);
-                $tickets2 = $this->check_diff_multi($old_tickets, $tickets);
-                $tickets = $tickets1 + $tickets2;
-                //$tickets = $this->unique_multidim_array($tickets, 'id');
-            }
-            
-            $this->session->set_tempdata('tickets', $tickets, 600);
-            $current_time = date($results['current_time']);
-            $newTime = date("Y-m-d H:i:s",strtotime("$current_time +10 minutes"));
-            $this->session->set_tempdata('exp_time', $newTime, 600);
-            $total = number_format($total, 2, '.', '');
-            $this->session->set_tempdata('total', $total, 600);
-        }
-        if(!$this->session->tempdata('tickets')){
-            $this->session->set_flashdata('expired', 'Session Expired!');
-            redirect('events/shop/'. $this->session->userdata('shortUrl'));
-        }
-   
-        $this->loadViews("events/your_tickets", $this->global, '', 'footerShop', 'headerShop');
 
     }
 
