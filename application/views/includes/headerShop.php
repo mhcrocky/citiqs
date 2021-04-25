739f966722f8546cb8793a6cc62c27be65cdaba5
aktwork <larastar721@gmail.com>
2021-04-26 01:19:07 +0200

    made small changes

--- application/views/includes/headerShop.php
+++ application/views/includes/headerShop.php
@@ -10,12 +10,11 @@
     <title><?php echo $pageTitle ? $pageTitle : 'TIQS | SHOP'; ?></title>
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo $this->baseUrl; ?>assets/bootstrap/bootstrap.min.css">
 
     <link rel="stylesheet" href="<?php echo $this->baseUrl; ?>assets/css/menu-style.css">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/izitoast/1.4.0/css/iziToast.css">
+    <link rel="stylesheet" href="<?php echo $this->baseUrl; ?>assets/css/iziToast.css">
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
