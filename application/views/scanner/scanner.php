98db51c9616ded17476f68c4ac954583074e8afe
aktwork <larastar721@gmail.com>
2021-05-17 21:23:02 +0200

    scanner functions js

--- application/views/scanner/scanner.php
+++ application/views/scanner/scanner.php
@@ -12,3 +12,14 @@
 <main>
     <input type="text" id="order" name="order" autofocus />
 </main>
+
+<script>
+    document.getElementById('order').addEventListener('change', function(){
+        console.dir('change');
+    });
+
+    document.getElementById('order').addEventListener('input', function(){
+        console.dir('input');
+    });
+
+</script>
