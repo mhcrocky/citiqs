c1df5c03a27922364a0baaa4e24cf0bcf4c5d3f4
aktwork <larastar721@gmail.com>
2021-05-18 00:08:25 +0200

    made a small change

--- application/views/events/inputs.php
+++ application/views/events/inputs.php
@@ -2,7 +2,7 @@
 <div class="row mt-5 p-4">
     <div class="col-md-4 mb-3">
         <div class="input-group">
-            <input type="button" value="<?php echo $this->language->tline('Add Tag'); ?>"
+            <input type="button" value="<?php echo $this->language->tline('Add Input'); ?>"
                 style="background: #10b981 !important;border-radius:0;height:45px;"
                 class="btn btn-primary form-control mb-3 text-left" data-toggle="modal" data-target="#addEventInputModal">
             <span style="background: #275C5D;padding-top: 14px;" class="input-group-addon pl-2 pr-2 mb-3"
