66c7cc58256c6cccf057a7c8c44b43a8d9dd256a
aktwork <larastar721@gmail.com>
2021-05-20 12:08:06 +0200

    modified datatable

--- assets/home/js/eventsDataTable.js
+++ assets/home/js/eventsDataTable.js
@@ -119,7 +119,8 @@ $(document).ready(function() {
                 data: null,
                 "render": function(data, type, row) {
                     
-                    let html = '<div style="display: inline-flex; width: 180px;justify-content: center;"><a class="event-icons mr-1" href="'+globalVariables.baseUrl+'events/edit/'+data.id+'"><i class="fa fa-pencil mx-auto"></i></a>';
+                    let html = '<div style="display: inline-flex; width: 240px;justify-content: center;"><a class="event-icons mr-1" href="javascript:;" style="padding-top: 10px;padding-left: 12px;" onclick="openShopUrlModal('+data.id+')" target="_blank" data-toggle="modal" data-target="#copyUrlToClipboard"><i class="gg-copy"></i></a>';
+                    html += '<a class="event-icons ml-3" href="'+globalVariables.baseUrl+'events/edit/'+data.id+'"><i class="fa fa-pencil mx-auto"></i></a>';
                     html += '<a class="event-icons ml-3" style="font-size: 19px;" href="'+globalVariables.baseUrl+'events/guestlist/'+data.id+'"><i class="gg-user-list"></i></a>'
                     html += '<a class="event-icons ml-3" href="'+globalVariables.baseUrl+'events/event/'+data.id+'"><i class="ti-ticket ticket-icon"></i></a></div>';
                     return html;
@@ -138,6 +139,7 @@ $(document).ready(function() {
                  }
 
             },
+            /*
             {
 
                 title: 'Url',
@@ -147,6 +149,7 @@ $(document).ready(function() {
                 }
 
             },
+            */
             {
 
                 title: 'Shop Design',
