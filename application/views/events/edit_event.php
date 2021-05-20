c15929105f2f281b7b0f179c23d9b0ac62b1e7c2
aktwork <larastar721@gmail.com>
2021-05-20 02:08:24 +0200

    added go back button

--- application/views/events/edit_event.php
+++ application/views/events/edit_event.php
@@ -2,15 +2,34 @@
     <div class="cotainer">
         <div class="row justify-content-center">
             <div class="col-md-12">
+
                 <div style="background: none;" class="card p-4">
 
                     <div class="card-body">
+
+                        <div class="col-md-2 ml-auto">
+                            <a href="<?php base_url(); ?>events">
+                                <div class="input-group">
+                                    <input type="button" value="<?php echo $this->language->tline('Go Back'); ?>"
+                                        style="background: #10b981 !important;border-radius:0;height:45px;"
+                                        class="btn btn-primary form-control mb-3 text-left" data-toggle="modal"
+                                        data-target="#guestlistModal">
+                                    <span style="background: #275C5D;padding-top: 14px;"
+                                        class="input-group-addon pl-2 pr-2 mb-3" data-toggle="modal"
+                                        data-target="#guestlistModal">
+                                        <i style="color: #fff;font-size: 18px;" class="ti ti-arrow-left"></i>
+                                    </span>
+
+                                </div>
+                            </a>
+                        </div>
+
                         <form id="my-form" name="my-form" class="needs-validation"
                             action="<?php echo base_url(); ?>events/update_event/<?php echo $event->id; ?>"
                             method="POST" enctype="multipart/form-data" novalidate>
                             <div class="form-group row">
                                 <label for="full_name" class="col-md-4 col-form-label text-md-left">
-                                    <h3>
+                                    <h3 style="margin-top: 0px !important">
                                         <strong>Create event</strong>
                                     </h3>
                                 </label>
@@ -22,8 +41,8 @@
                                 <label for="event-name" class="col-md-4 col-form-label text-md-left">Event Name</label>
                                 <div class="col-md-8">
 
-                                    <input type="text" id="event-name" class="input-w form-control border-50" name="eventname"
-                                        value="<?php echo $event->eventname; ?>" required>
+                                    <input type="text" id="event-name" class="input-w form-control border-50"
+                                        name="eventname" value="<?php echo $event->eventname; ?>" required>
 
                                 </div>
                             </div>
@@ -46,11 +65,12 @@
                                     <label class="file">
                                         <input type="file" name="userfile" id="file" onchange="editImageUpload(this)"
                                             aria-label="File browser example">
-                                        <span id="imageUpload" class="file-custom" data-content="Choose image ..."></span>
+                                        <span id="imageUpload" class="file-custom"
+                                            data-content="Choose image ..."></span>
                                     </label>
                                     <div style="padding-left: 0;" class="col-sm-6">
                                         <?php if($event->eventImage == ''): ?>
-                                            <img src="<?php echo base_url(); ?>assets/images/img-preview.png" id="preview"
+                                        <img src="<?php echo base_url(); ?>assets/images/img-preview.png" id="preview"
                                             class="img-thumbnail">
                                         <?php else: ?>
                                         <img src="<?php echo base_url(); ?>assets/images/events/<?php echo $event->eventImage; ?>"
@@ -67,28 +87,31 @@
                             </div>
 
                             <div class="form-group row">
-                                <label for="image" class="col-md-4 col-form-label text-md-left">Upload Background Image</label>
+                                <label for="image" class="col-md-4 col-form-label text-md-left">Upload Background
+                                    Image</label>
                                 <div class="col-md-8">
 
 
                                     <label class="file">
                                         <input type="file" class="border-50" name="backgroundfile" id="background-file"
                                             onchange="editBackgroundUpload(this)" aria-label="File browser">
-                                        <span class="file-custom" id="img-background" data-content="Choose image ..."></span>
+                                        <span class="file-custom" id="img-background"
+                                            data-content="Choose image ..."></span>
                                     </label>
                                     <div style="padding-left: 0;" class="col-sm-6">
                                         <?php if($event->backgroundImage == ''): ?>
-                                        <img src="<?php echo base_url(); ?>assets/images/img-preview.png" id="background-preview"
-                                            class="img-thumbnail">
+                                        <img src="<?php echo base_url(); ?>assets/images/img-preview.png"
+                                            id="background-preview" class="img-thumbnail">
                                         <?php else: ?>
-                                            <img src="<?php echo base_url(); ?>assets/images/events/<?php echo $event->backgroundImage; ?>" id="background-preview"
-                                            class="img-thumbnail">
+                                        <img src="<?php echo base_url(); ?>assets/images/events/<?php echo $event->backgroundImage; ?>"
+                                            id="background-preview" class="img-thumbnail">
                                         <?php endif; ?>
                                     </div>
 
-                                    <input type="hidden" id="backgroundImgChanged" value="false" name="backgroundImgChanged">
-                                    <input type="hidden" id="backgroundImgName" value="<?php echo $event->backgroundImage; ?>"
-                                        name="backgroundImgName">
+                                    <input type="hidden" id="backgroundImgChanged" value="false"
+                                        name="backgroundImgChanged">
+                                    <input type="hidden" id="backgroundImgName"
+                                        value="<?php echo $event->backgroundImage; ?>" name="backgroundImgName">
 
 
                                 </div>
@@ -175,16 +198,17 @@
                             <div class="form-group row">
                                 <label for="venue" class="col-md-4 col-form-label text-md-left">Venue</label>
                                 <div class="col-md-6">
-                                    <input type="text" id="venue" class="form-control border-50 input-w" name="eventVenue"
-                                        placeholder="Enter a location" value="<?php echo $event->eventVenue; ?>"
-                                        required>
+                                    <input type="text" id="venue" class="form-control border-50 input-w"
+                                        name="eventVenue" placeholder="Enter a location"
+                                        value="<?php echo $event->eventVenue; ?>" required>
                                 </div>
                             </div>
 
                             <div class="form-group row wideField">
                                 <label for="address" class="col-md-4 col-form-label text-md-left">Address</label>
                                 <div class="col-md-6">
-                                    <input type="text" id="autocomplete" onFocus="geolocate()" class="field form-control border-50 input-w" name="eventAddress"
+                                    <input type="text" id="autocomplete" onFocus="geolocate()"
+                                        class="field form-control border-50 input-w" name="eventAddress"
                                         value="<?php echo $event->eventAddress; ?>" required>
                                 </div>
                             </div>
@@ -192,8 +216,8 @@
                             <div class="form-group row wideField">
                                 <label for="city" class="col-md-4 col-form-label text-md-left">City</label>
                                 <div class="col-md-6">
-                                    <input type="text" id="locality" class="field form-control border-50 input-w" name="eventCity"
-                                        value="<?php echo $event->eventCity; ?>" required>
+                                    <input type="text" id="locality" class="field form-control border-50 input-w"
+                                        name="eventCity" value="<?php echo $event->eventCity; ?>" required>
                                 </div>
                             </div>
 
@@ -201,8 +225,8 @@
                                 <label for="postal-code" class="col-md-4 col-form-label text-md-left">Postal
                                     code</label>
                                 <div class="col-md-6">
-                                    <input type="text" id="postal_code" class="field form-control border-50 input-w" name="eventZipcode"
-                                        value="<?php echo $event->eventZipcode; ?>" required>
+                                    <input type="text" id="postal_code" class="field form-control border-50 input-w"
+                                        name="eventZipcode" value="<?php echo $event->eventZipcode; ?>" required>
                                 </div>
                             </div>
 
@@ -210,12 +234,14 @@
                                 <label for="country" class="col-md-4 col-form-label text-md-left">Country
                                 </label>
                                 <div class="col-md-6 font-weight-bold">
-                                    <select id="country" name="eventCountry" class="field form-control border-50 input-w font-weight-bold" required>
+                                    <select id="country" name="eventCountry"
+                                        class="field form-control border-50 input-w font-weight-bold" required>
                                         <option class="text-weight-bold" value="">Select option</option>
                                         <?php if(count($countries) > 0): ?>
                                         <?php foreach($countries as $country): ?>
                                         <?php if($event->eventCountry == $country): ?>
-                                        <option class="font-weight-bold" value="<?php echo $country; ?>" selected="selected">
+                                        <option class="font-weight-bold" value="<?php echo $country; ?>"
+                                            selected="selected">
                                             <?php echo $country; ?>
                                         </option>
                                         <?php else: ?>
@@ -232,8 +258,9 @@
                                 </label>
                                 <div class="col-md-6">
                                     <div class="input-group date">
-                                        <input type="text" class="form-control inp-group-radius-left input-w input-date" id="event-date1"
-                                            name="StartDate" value="<?php echo date('d-m-Y', strtotime($event->StartDate)); ?>" required>
+                                        <input type="text" class="form-control inp-group-radius-left input-w input-date"
+                                            id="event-date1" name="StartDate"
+                                            value="<?php echo date('d-m-Y', strtotime($event->StartDate)); ?>" required>
                                         <input type="time" class="form-control input-w" id="event-time1"
                                             name="StartTime" value="<?php echo $event->StartTime; ?>" required>
                                         <span class="input-group-addon fa-input pl-2 pr-2">
@@ -257,10 +284,13 @@
                                 </label>
                                 <div class="col-md-6">
                                     <div class="input-group date">
-                                        <input type="text" class="form-control inp-group-radius-left input-w input-date" id="event-date2"
-                                            name="EndDate" value="<?php echo date('d-m-Y', strtotime($event->EndDate)); ?>" onchange="checkTimestamp()" onfocus="timestampOnFocus()" required>
+                                        <input type="text" class="form-control inp-group-radius-left input-w input-date"
+                                            id="event-date2" name="EndDate"
+                                            value="<?php echo date('d-m-Y', strtotime($event->EndDate)); ?>"
+                                            onchange="checkTimestamp()" onfocus="timestampOnFocus()" required>
                                         <input type="time" class="form-control input-w" id="event-time2" name="EndTime"
-                                            value="<?php echo $event->EndTime; ?>" onchange="checkTimestamp()" onfocus="timestampOnFocus()" required>
+                                            value="<?php echo $event->EndTime; ?>" onchange="checkTimestamp()"
+                                            onfocus="timestampOnFocus()" required>
                                         <span class="input-group-addon fa-input pl-2 pr-2">
                                             <i style="color: #fff;font-size: 18px;" class="fa fa-calendar"></i></span>
                                     </div>
@@ -309,7 +339,6 @@
     let country = '<?php echo  $event->eventCountry; ?>';
     $('#country').val(country);
 }());
-
 </script>
 
 <?php endif; ?>
