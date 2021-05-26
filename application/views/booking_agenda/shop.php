afe65f128d3f099c895c6740af913f03ef006637
aktwork <larastar721@gmail.com>
2021-05-26 17:38:26 +0200

    cleaned view file

--- application/views/booking_agenda/shop.php
+++ application/views/booking_agenda/shop.php
@@ -1,70 +1,4 @@
 <!-- HERO SECTION -->
-<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/@splidejs/splide@latest/dist/css/splide.min.css'>
-
-<style>
-.splide {
-    padding: 20px 0;
-}
-
-.splide__slide img {
-    display: block;
-    width: 100%;
-    border-radius: 8px;
-    transition: transform 400ms;
-    transform: scale(0.9);
-    transform-origin: center center;
-}
-
-.splide__slide.is-active img {
-    transform: scale(1);
-}
-
-.splide .splide__arrow {
-    top: 0;
-    bottom: 0;
-    height: 100%;
-    transform: none;
-    border-radius: unset;
-    width: 50px;
-    opacity: 0.9;
-}
-
-.splide .splide__arrow svg {
-    filter: invert(1);
-    width: 24px;
-    height: 24px;
-}
-
-.splide__arrow.splide__arrow--prev {
-    left: 0;
-    background: linear-gradient(90deg, rgba(212, 231, 53, 0.7) 0%, rgba(212, 231, 53, 0) 100%);
-}
-
-.splide__arrow.splide__arrow--next {
-    right: 0;
-    background: linear-gradient(270deg, rgba(212, 231, 53, 0.7) 0%, rgba(212, 231, 53, 0) 100%);
-}
-
-
-
-
-
-.left {
-    text-align: left;
-}
-
-@media only screen and (max-width: 400px) {
-
-.flatpickr-weekdays, .flatpickr-day {
-    width: 250px !important;
-}
-.dayContainer {
-    width: 250px !important;
-    min-width: 250px !important;
-    max-width: 250px !important;
-    }
-}
-</style>
 
 <?php if($this->session->flashdata('expired')): ?>
 <div class="alert alert-danger alert-dismissible fade show" role="alert">
@@ -135,7 +69,7 @@
                 </div>
             </div>
         </div>
-        
+
 
 
 
@@ -191,158 +125,12 @@
     </div>
 </section>
 <!-- END HERO SECTION -->
-<script src='https://cdn.jsdelivr.net/npm/@splidejs/splide@2.4.6/dist/js/splide.min.js'></script>
-<script id="rendered-js">
-document.addEventListener('DOMContentLoaded', function() {
-    new Splide('#splide', {
-        type: 'slide',
-        perPage: 3,
-        focus: 'center',
-        autoplay: false,
-        interval: 8000,
-        flickMaxPages: 3,
-        updateOnMove: true,
-        pagination: false,
-        padding: '10%',
-        throttle: 300,
-        breakpoints: {
-            1440: {
-                perPage: 1,
-                padding: '30%'
-            }
-        }
-    }).
 
 
-    mount();
-});
-//# sourceURL=pen.js
-</script>
 <script>
 (function() {
     changeTextContent();
-
-
-
-
-
-
 }());
-</script>
 
-
-<script src='https://cdn.jsdelivr.net/npm/flatpickr'></script>
-<script>
-// generate events
-var agendas = '<?php echo json_encode($agendas); ?>';
-agendas = JSON.parse(agendas);
-console.log(agendas);
-
-var eventDates = {};
-
-if (agendas.length > 0) {
-    $.each(agendas, function(index, agenda) {
-        let dateTime = agenda.ReservationDateTime.split(' ');
-        let day = dateTime[0];
-        eventDates[day] = [
-            agenda.id
-        ];
-    });
-}
-
-
-
-// set maxDates
-var maxDate = {
-    1: new Date(new Date().setMonth(new Date().getMonth() + 11)),
-    2: new Date(new Date().setMonth(new Date().getMonth() + 10)),
-    3: new Date(new Date().setMonth(new Date().getMonth() + 9))
-};
-
-
-var flatpickr = $('#calendar .placeholder').flatpickr({
-    inline: true,
-    minDate: 'today',
-    maxDate: maxDate[3],
-
-    showMonths: 1,
-    enable: Object.keys(eventDates),
-    disableMobile: "true",
-    onChange: function(date, str, inst) {
-        let agendaId = '';
-        if (date.length) {
-            console.log(eventDates[str].length);
-            for (i = 0; i < eventDates[str].length; i++) {
-                if (typeof window.CP !== 'undefined' && window.CP.shouldStopExecution(0)) break;
-                agendaId = eventDates[str][i];
-
-                return getSpotsView(agendaId);
-            }
-        }
-    },
-    locale: {
-        weekdays: {
-            shorthand: ["S", "M", "T", "W", "T", "F", "S"],
-            longhand: [
-                "Sunday",
-                "Monday",
-                "Tuesday",
-                "Wednesday",
-                "Thursday",
-                "Friday",
-                "Saturday"
-            ]
-        }
-    }
-});
-
-
-
-
-
-eventCaledarResize($(window));
-$(window).on('resize', function() {
-    eventCaledarResize($(this));
-});
-
-function eventCaledarResize($el) {
-    var width = $el.width();
-    if (width < 400) {
-        flatpickr.set('showMonths', 1);
-
-        flatpickr.set('maxDate', maxDate[0]);
-        $('.flatpickr-calendar').css('width', '200px');
-    }
-
-    if (flatpickr.selectedDates.length) {
-        flatpickr.clear();
-    }
-
-    if (width >= 992 && flatpickr.config.showMonths !== 3) {
-        flatpickr.set('showMonths', 1);
-        flatpickr.set('maxDate', maxDate[1]);
-        $('.flatpickr-calendar').css('width', '');
-    }
-    
-    if (width < 992 && width >= 768 && flatpickr.config.showMonths !== 2) {
-        flatpickr.set('showMonths', 1);
-        console.log(width);
-        flatpickr.set('maxDate', maxDate[1]);
-        $('.flatpickr-calendar').css('width', '');
-    }
-    if (width < 768 && flatpickr.config.showMonths !== 1) {
-        console.log(width);
-        flatpickr.set('showMonths', 1);
-        flatpickr.set('maxDate', maxDate[1]);
-        $('.flatpickr-calendar').css('width', '');
-    }
-    
-}
-
-function formatDate(date) {
-    let d = date.getDate();
-    let m = date.getMonth() + 1; //Month from 0 to 11
-    let y = date.getFullYear();
-    return '' + y + '-' + (m <= 9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d);
-}
+const agendaList = '<?php echo json_encode($agendas); ?>';
 </script>
