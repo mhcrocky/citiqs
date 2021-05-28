1df52f18eb9483a5b91d1193c66ff85b9629446a
aktwork <larastar721@gmail.com>
2021-05-28 14:01:49 +0200

    keep session after reservation done

--- application/controllers/Bookingpay.php
+++ application/controllers/Bookingpay.php
@@ -721,27 +721,22 @@ class Bookingpay extends BaseControllerWeb
                 $this->bookandpay_model->updateBookandpayByTransactionId($transactionid);
                 $this->emailReservation($transactionid);
             }
-
             $statuscode = intval($this->input->get('orderStatusId'));
             if ($statuscode == 100) {
                 $data = array();
                 $this->global['pageTitle'] = 'TIQS : THANKS';
-                $this->session->sess_destroy();
                 $this->loadViews("bookingsuccess", $this->global, $data, 'nofooter', "noheader");
             } elseif ($statuscode <0) {
                 $data = array();
                 $this->global['pageTitle'] = 'TIQS : THANKS';
-                $this->session->sess_destroy();
                 $this->loadViews("thuishavenerror", $this->global, $data, 'nofooter', "noheader");
             } elseif ($statuscode >= 0) {
                 $data = array();
 			    $this->global['pageTitle'] = 'TIQS : THANKS';
-			    $this->session->sess_destroy();
 			    $this->loadViews("thuishavenerror", $this->global, $data, 'nofooter', "noheader");
             } else {
 			    $data = array();
 			    $this->global['pageTitle'] = 'TIQS : THANKS';
-			    $this->session->sess_destroy();
 			    $this->loadViews("thuishavenerror", $this->global, $data, 'nofooter', "noheader");
             }
 
