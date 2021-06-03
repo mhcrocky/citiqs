ae5bc0fe205092829e8a55ebd08061c7e8d7a2f0
aktwork <larastar721@gmail.com>
2021-06-03 23:11:29 +0200

    added custom route

--- application/config/routes.php
+++ application/config/routes.php
@@ -564,6 +564,9 @@ $route['customer_panel/settings']="Customer_panel/settings";
 $route['customer_panel/list_templates']="Customer_panel/listTemplates";
 $route['customer_panel/get_email_template']="Customer_panel/get_email_template";
 $route['customer_panel/spots_order']="Customer_panel/spots_order";
+$route['customer_panel/financial_report']="Customer_panel/financial_report";
+$route['customer_panel/get_financial_report']="Customer_panel/get_financial_report";
+
 
 $route['settingsmenu/savespotobject']="Settingsmenu/saveSpotObject";
 
