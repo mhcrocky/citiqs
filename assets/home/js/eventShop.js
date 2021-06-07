f9788f7c9f477cd94256c072c76c0a200f0e0db0
aktwork <larastar721@gmail.com>
2021-06-07 17:35:22 +0200

    fixed a issue with background image

--- assets/home/js/eventShop.js
+++ assets/home/js/eventShop.js
@@ -158,7 +158,7 @@ function getTicketsView(eventId, first = false) {
         }
 
         if(isSquared == '1'){
-            $('.hero__background').attr('style', 'clip-path: none !important; width: auto !important');
+            $('.hero__background').attr('style', 'clip-path: none !important; width: auto !important; max-width: 65%;');
         } else {
             $('.hero__background').attr('style', '');
         }
