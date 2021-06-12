e1c754d9651b215fe6b8d41d37439c298dd87b3b
aktwork <larastar721@gmail.com>
2021-06-13 00:38:52 +0200

    first commit

---
+++ application/views/events/scannedin_graph.php
@@ -0,0 +1,20 @@
+<style>
+.panel-heading {
+    padding: 10px;
+}
+.breadcrumb {
+    margin: 10px;
+}
+.drilldown-level {
+    background: #fff;
+    padding: 10px;
+}
+</style>
+<div style="margin-top: 3%;" class="main-content-inner ui-sortable">
+    
+    <div class="row-sort ui-sortable" style="background:#ddd;margin-bottom: 3%;" data-row_position="3" data-row_sort="1">
+        <?php echo $scan_graphs; ?> 
+    </div>
+  
+</div>
+
