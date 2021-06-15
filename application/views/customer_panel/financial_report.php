d6684e26dc6aa51a7a655b7102b3ebd7f41fe1e3
aktwork <larastar721@gmail.com>
2021-06-15 16:31:37 +0200

    made a small change

--- application/views/customer_panel/financial_report.php
+++ application/views/customer_panel/financial_report.php
@@ -1,7 +1,7 @@
 
     <div style="padding:25px;" class="w-100 mt-3 row-sort ui-sortable">
         <div class="float-right text-center pl-3">
-            <input style="width: 330px;" class="date form-control-sm mb-2" type="text" name="datetimes"
+            <input style="width: 220px;" class="date form-control-sm mb-2" type="text" name="datetimes"
                 id="reportDateTime" />
             <!--
             <select style="width: 264px;font-size: 14px;"
