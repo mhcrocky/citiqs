794b6c159be3581fae0853a0f4150614d335385d
aktwork <larastar721@gmail.com>
2021-06-28 06:19:59 +0200

    made small change

--- application/views/customeremail/index.php
+++ application/views/customeremail/index.php
@@ -147,7 +147,7 @@
 
                     <div class="form-group row">
                         <label for="email" class="col-md-4 col-form-label text-md-left">
-                            Email Template
+                            Campaign
                         </label>
                         <div class="col-md-6">
 
