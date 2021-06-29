8b47d9d35ce2d00ce14f6f3c3cc880f39376aab4
aktwork <larastar721@gmail.com>
2021-06-30 01:18:57 +0200

    added koolreport for tickets

--- application/controllers/Buyer.php
+++ application/controllers/Buyer.php
@@ -2,12 +2,19 @@
     declare(strict_types=1);
 
     if (!defined('BASEPATH')) exit('No direct script access allowed');
+    
+    include(APPPATH . '/libraries/koolreport/core/autoload.php');
 
     require APPPATH . '/libraries/BaseControllerWeb.php';
+    
+    use \koolreport\drilldown\DrillDown;
+    use \koolreport\widgets\google\ColumnChart;
+    use \koolreport\clients\Bootstrap4;
+    use \koolreport\bootstrap3\Theme;
 
     class Buyer extends BaseControllerWeb
     {
-
+        private $buyerId;
         public function __construct()
         {
             parent::__construct();
@@ -17,11 +24,13 @@
             $this->load->helper('utility_helper');
 
             $this->load->model('shoporder_model');
+            $this->load->model('event_model');
 
             $this->load->library('language', array('controller' => $this->router->class));
             $this->load->library('session');
 
             $this->load->config('custom');
+            $buyerId = $this->session->userdata('buyerId');
         }
 
         private function checkIsBuyer(): void
@@ -49,9 +58,143 @@
         {
             $this->global['pageTitle'] = 'TIQS : BUYER TICKETS';
 
-            $data = [];
+            $this->global['pageTitle'] = 'TIQS: Tags Graphs';
+            $reservations = $this->event_model->get_reservations_stats_by_tags_for_buyer($this->buyerId);
+            $events = $this->event_model->get_all_events($this->buyerId);
+            $data['events'] = $events;
+            
+            $data['graph'] = isset($events[0]) ? $this->get_tags_stats($events[0]['id'], true) : [];
 
             $this->loadViews('buyer/buyerTickets', $this->global, $data, 'footerbusiness', 'headerbusiness');
+        }
+
+        public function get_tags_stats($eventId = false, $dateRange = false)
+        {
+
+            $issetEventId = ($eventId) ? true : false;
+            $eventId = ($eventId) ? $eventId : $this->input->post('eventId');
+            $dateRange = (!$dateRange) ? urldecode($this->input->post('dateRange')) : '0000-00-00 - 0000-00-00';
+            $dateRange = explode(' - ', $dateRange);
+
+            $GLOBALS['eventId'] = $eventId;
+            $GLOBALS['startDate'] = $dateRange[0] . ' 00:00:00';
+            $GLOBALS['endDate'] = $dateRange[1] . ' 23:59:59';
+
+            $graph = DrillDown::create(array(
+                "name" => "saleDrillDown",
+                "title" => "Tags Stats",
+                "levels" => array(
+                    array(
+                        "title" => "Tickets Sold",
+                            "content" => function ($params, $scope) {
+                                global $eventId;
+                                global $startDate;
+                                global $endDate;
+
+                            $conditions = [
+                                'eventId' => $eventId,
+                                'startDate' => $startDate,
+                                'endDate' => $endDate
+                            ];
+
+                            $tags = $this->event_model->get_tags_ticket_sold_stats_for_buyer($this->buyerId, $eventId, $startDate, $endDate);
+
+                            $columnArr['date'] = [
+                                "type" => "string",
+                                "label" => "Date",
+                            ];
+
+                            foreach($tags as $tag){
+                                $keys = array_keys($tag);
+                                foreach($keys as $key){
+                                    if($key == 'date') { continue; }
+                                    $tag = $key;
+                                    $columnArr[$tag] = [
+                                        "label" => $tag
+                                    ];
+                                }
+                            
+                            }
+
+                            $tags = array_values($tags);
+
+
+                        
+                            ColumnChart::create(array(
+                                "dataSource" => $tags, 
+                                "columns" => $columnArr,
+                                "clientEvents" => array(
+                                    "itemSelect" => "function(params){
+                                        saleDrillDown.next({conditions:" . json_encode($conditions) . "});
+                                    }",
+                                ),
+                                "options"=>array(
+                                    "isStacked"=>true
+                                )
+                            ));
+                        }
+                    ),
+
+                    array(
+                        "title" => function ($params, $scope) {
+                            return "Amount";
+                        },
+                        "content" => function ($params, $scope) {
+
+                            $conditions = $params['conditions'];
+                            $eventId = $conditions['eventId'];
+                            $startDate = $conditions['startDate'];
+                            $endDate = $conditions['endDate'];
+
+                            $tags = $this->event_model->get_tags_amount_stats_for_buyer($this->buyerId, $eventId, $startDate, $endDate);
+
+                            $tags = array_values($tags);
+
+                            $columnArr['date'] = [
+                                "type" => "string",
+                                "label" => "Date",
+                            ];
+
+                            foreach($tags as $tag){
+                                $keys = array_keys($tag);
+                                foreach($keys as $key){
+                                    if($key == 'date') { continue; }
+                                    $tag = $key;
+                                    $columnArr[$tag] = [
+                                        "label" => $tag,
+                                        "type"=>"number",
+                                        "prefix"=>"€"
+                                    ];
+                                }
+                            
+                            }
+
+
+                            ColumnChart::create(array(
+                                "dataSource" => $tags, 
+                                "columns" => $columnArr,
+                                "clientEvents" => array(
+                                    "itemSelect" => "function(params){
+                                        saleDrillDown.next({date:params.selectedRow[0]});
+                                    }",
+                                ),
+                                "options"=>array(
+                                    "isStacked"=>true
+                                )
+                           
+                            ));
+                        }
+                    ),
+
+                ),
+                "themeBase" => "bs4",
+            ), true);
+
+            if($issetEventId){
+                return $graph;
+            }
+
+            echo json_encode($graph);
         }
 
 
--- application/views/buyer/buyerTickets.php
+++ application/views/buyer/buyerTickets.php
@@ -1,1 +1,58 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+
+<main class="my-form">
+    <div class="cotainer">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div style="background: none;" class="card p-4">
+
+                    <div class="card-body">
+
+                    
+                    
+                        <div class="row">
+
+
+                            <!-- Events Select Box -->
+                            <div class="col-md-9">
+                                <select id="events" name="events" onchange="getEventGraph()"
+                                        style="background-color: #f4f3f3 !important; border-radius: 0px !important;font-weight: 510;"
+                                        class="form-control w-100 mb-5" required>
+                                    <?php if(is_array($events) && count($events) > 0): ?>
+                                        <?php foreach($events as $event): ?>
+                                            <option class="font-weight-bold" value="<?php echo $event['id']; ?>"><?php echo date('d/m/Y', strtotime($event['StartDate'])); ?> - <?php echo $event['eventname']; ?></option>
+                                        <?php endforeach; ?>
+                                    <?php endif; ?>
+                                </select>
+                            </div>
+                            <!-- End Events Select Box -->
+
+                            <!-- Datetime Picker Box -->
+                            <div class="col-md-3">
+                                <input style="border-radius: 0px !important;" onchange="getEventGraph()" class="form-control mb-2 w-100" type="text" name="datetimes" />
+                            </div>
+                            <!-- Datetime Picker Box -->
+
+
+
+                            <!-- Graph -->
+                            <div id="graph" style="background: #fff; visibility: hidden" class="w-100">
+                                <?php echo isset($graph) ? $graph : ''; ?>
+                            </div>
+                            <!-- End Graph -->
+
+                            
+                        
+                        </div>
+
+
+
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
