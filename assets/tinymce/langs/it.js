d63d02e7f98b72878e369fa91fa14760efaff069
aktwork <larastar721@gmail.com>
2021-06-30 14:08:14 +0200

    tinymce no cdn

---
+++ assets/tinymce/langs/it.js
@@ -0,0 +1,16 @@
+tinymce.addI18n('it',{
+	'Choose YouTube Video'  : 'Cerca YouTube Video',
+	'Insert Youtube video'  : 'Inserisci video di Youtube',
+	'width'					: 'Larghezza',
+	'height'				: 'Altezza',
+	'skin'					: 'Pelle',
+	'dark'          		: 'buio',
+	'light'         		: 'leggero',
+	'Search'        		: 'Ricerca',
+	'Youtube URL'   		: 'URL di Youtube',
+	'Title'         		: 'Titolo',
+	'Insert and Close'		: 'Inserire e chiudere',
+	'Insert'				: 'Inserire',
+	'Load More'				: 'Carica pi\U00F9',   
+	'cancel'				: 'Annulla'
+});
