e4d772cf6afb7c7ae8164ffe37903e6a575e9018
aktwork <larastar721@gmail.com>
2021-07-02 09:07:19 +0200

    unset session messages

--- application/views/includes/alertifySessionMessage.php
+++ application/views/includes/alertifySessionMessage.php
@@ -1,21 +1,30 @@
 
 <?php
     $fail =  $this->session->flashdata('fail');  
-    if($fail) {
+    if ($fail) {
 ?>
     <script>alertify.error('<?php echo $fail; ?>');</script>
 <?php 
     }
     $error = $this->session->flashdata('error');
-    if($error) {
+    if ($error) {
 ?>
     <script>alertify.error('<?php echo $error; ?>');</script>
 <?php
     }
     $success = $this->session->flashdata('success');
-    if($success){
+    if ($success) {
 ?>
     <script>alertify.success('<?php echo $success; ?>');</script>
 <?php 
+    }
+    if (!empty($_SESSION['success'])) {
+        unset($_SESSION['success']);
+    }
+    if (!empty($_SESSION['fail'])) {
+        unset($_SESSION['fail']);
+    }
+    if (!empty($_SESSION['error'])) {
+        unset($_SESSION['error']);
     }
 ?>
